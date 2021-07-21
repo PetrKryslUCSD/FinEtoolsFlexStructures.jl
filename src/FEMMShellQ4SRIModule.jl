5c5cacc5370fc8f42e5131524e7140621b834c4f
Petr Krysl <pkrysl@ucsd.edu>
2021-07-21 14:17:30 -0700

    fixed element length

--- src/FEMMShellQ4SRIModule.jl
+++ src/FEMMShellQ4SRIModule.jl
@@ -1,6 +1,6 @@
 module FEMMShellQ4SRIModule
 
-using LinearAlgebra: norm, Transpose, mul!, diag, rank, eigen
+using LinearAlgebra: norm, Transpose, mul!, diag, rank, eigen, cross
 using Statistics: mean
 using FinEtools
 import FinEtools.FESetModule: gradN!, nodesperelem, manifdim
@@ -117,6 +117,13 @@ function _compute_J0!(J0, ecoords)
     x, y, z = ((ecoords[3, :].+ecoords[4, :])./2 .- (ecoords[2, :].+ecoords[1, :])./2) ./ 2
     J0[:, 2] .= (x, y, z)
 end
+
+function element_size(J0)
+    a = @view J0[:, 1]
+    b = @view J0[:, 2]
+    @show 4*sqrt(sum((-a[3]*b[2]+a[2]*b[3], a[3]*b[1]-a[1]*b[3], -a[2]*b[1]+a[1]*b[2]).^2))
+    sqrt(4*sqrt(sum((-a[3]*b[2]+a[2]*b[3], a[3]*b[1]-a[1]*b[3], -a[2]*b[1]+a[1]*b[2]).^2)))
+end
     
 function _shell_material_stiffness(material)
     D = fill(0.0, 6, 6)
@@ -194,12 +201,14 @@ Compute the material stiffness matrix.
 function stiffness(self::FEMMShellQ4SRI, assembler::ASS, geom0::NodalField{FFlt}, u1::NodalField{T}, Rfield1::NodalField{T}, dchi::NodalField{TI}) where {ASS<:AbstractSysmatAssembler, T<:Number, TI<:Number}
     fes = self.integdomain.fes
     npts,  Ns,  gradNparams,  w,  pc = integrationdata(self.integdomain);
+    N0 = similar(Ns[1])
+    N0 .= 0.25
     loc, J, J0 = self._loc, self._J, self._J0
     ecoords0, ecoords1, edisp1, dofnums = self._ecoords0, self._ecoords1, self._edisp1, self._dofnums
     lecoords0 = self._lecoords0
     F0, Ft, FtI, FtJ, Te = self._F0, self._Ft, self._FtI, self._FtJ, self._Te
     R1I, R1J = self._RI, self._RJ
-    elmat, elmatTe = self._elmat, self._elmatTe
+    elmat, elmatTe, elmato = self._elmat, self._elmatTe, self._elmato
     lloc, lJ, lgradN = self._lloc, self._lJ, self._lgradN 
     Bm, Bb, Bs, Bsavg, DpsBmb, DtBs = self._Bm, self._Bb, self._Bs, self._Bsavg, self._DpsBmb, self._DtBs
     Dps, Dt = _shell_material_stiffness(self.material)
@@ -211,14 +220,18 @@ function stiffness(self::FEMMShellQ4SRI, assembler::ASS, geom0::NodalField{FFlt}
         gathervalues_asmat!(u1, edisp1, fes.conn[i]);
         ecoords1 .= ecoords0 .+ edisp1
         _compute_J0!(J0, ecoords0)
+        he = element_size(J0)
+        t = self.integdomain.otherdimension(loc, fes.conn[i], N0)
+        @show J0, he, he/t
+        alpha = 1.0
         local_frame!(delegateof(fes), Ft, J0)
         fill!(elmat,  0.0); # Initialize element matrix
+        fill!(elmato,  0.0); # Initialize element matrix
         volume = 0.0
         Bsavg .= 0.0
         for j in 1:npts
             locjac!(loc, J, ecoords0, Ns[j], gradNparams[j])
             Jac = Jacobiansurface(self.integdomain, J, loc, fes.conn[i], Ns[j]);
-            t = self.integdomain.otherdimension(loc, fes.conn[i], Ns[j])
             mul!(lecoords0, ecoords0, view(Ft, :, 1:2))
             locjac!(lloc, lJ, lecoords0, Ns[j], gradNparams[j])
             gradN!(fes, lgradN, gradNparams[j], lJ);
@@ -227,12 +240,14 @@ function stiffness(self::FEMMShellQ4SRI, assembler::ASS, geom0::NodalField{FFlt}
             _Bsmat!(Bs, lgradN, Ns[j])
             add_btdb_ut_only!(elmat, Bm, t*Jac*w[j], Dps, DpsBmb)
             add_btdb_ut_only!(elmat, Bb, (t^3)/12*Jac*w[j], Dps, DpsBmb)
+            add_btdb_ut_only!(elmato, Bs, alpha*t*Jac*w[j], Dt, DtBs)
             Bsavg .+= (Jac*w[j]*t).*Bs
             volume += (Jac*w[j]*t)
         end
         # Computed the average transverse sheer strain-displacement matrix
         Bsavg .*= (1/volume)
-        add_btdb_ut_only!(elmat, Bsavg, volume, Dt, DtBs)
+        add_btdb_ut_only!(elmat, Bsavg, (1-alpha)*volume, Dt, DtBs)
+        elmat .+= elmato
         # Apply drilling-rotation artificial stiffness
         kavg4 = (elmat[4, 4]+elmat[10, 10]+elmat[16, 16]+elmat[22, 22])/__nn
         kavg5 = (elmat[5, 5]+elmat[11, 11]+elmat[17, 17]+elmat[23, 23])/__nn
