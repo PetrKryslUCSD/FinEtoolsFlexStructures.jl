16b0d28925b068b6f1c1ae4d3201f514a600bc24
Petr Krysl <pkrysl@ucsd.edu>
2021-07-20 17:19:20 -0700

    with q4sri

--- examples/shells/deformation/clamped_hypar_examples.jl
+++ examples/shells/deformation/clamped_hypar_examples.jl
@@ -15,9 +15,11 @@ module clamped_hypar_examples
 using LinearAlgebra
 using FinEtools
 using FinEtoolsDeforLinear
-using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3, local_frame!
-using FinEtoolsFlexStructures.FEMMShellDSG3Module: FEMMShellDSG3, stiffness
-# using FinEtoolsFlexStructures.FEMMShellT3Module: FEMMShellT3, stiffness
+using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
+using FinEtoolsFlexStructures.FESetShellQ4Module: FESetShellQ4
+using FinEtoolsFlexStructures.FEMMShellDSG3Module
+# using FinEtoolsFlexStructures.FEMMShellT3Module
+using FinEtoolsFlexStructures.FEMMShellQ4SRIModule
 using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, linear_update_rotation_field!, update_rotation_field!
 using FinEtoolsFlexStructures.VisUtilModule: plot_nodes, plot_midline, render, plot_space_box, plot_midsurface, space_aspectratio, save_to_json
 
@@ -39,7 +41,7 @@ function single_dsg3()
     analyt_sol=-6.3941e-5;
     g = 80*0.1^3#
     
-# Mesh
+    # Mesh
     n = 8
     tolerance = L/n/1000
     fens, fes = T3block(L,L,n,n);
@@ -53,17 +55,18 @@ function single_dsg3()
     
     sfes = FESetShellT3()
     accepttodelegate(fes, sfes)
-    femm = FEMMShellDSG3(IntegDomain(fes, TriRule(1), thickness), mater)
+    femm = FEMMShellDSG3Module.FEMMShellDSG3(IntegDomain(fes, TriRule(1), thickness), mater)
+    stiffness = FEMMShellDSG3Module.stiffness
 
-# Construct the requisite fields, geometry and displacement
-# Initialize configuration variables
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
     geom0 = NodalField(fens.xyz)
     u0 = NodalField(zeros(size(fens.xyz,1), 3))
     Rfield0 = initial_Rfield(fens)
     dchi = NodalField(zeros(size(fens.xyz,1), 6))
 
-# Apply EBC's
-# Clamped edge
+    # Apply EBC's
+    # Clamped edge
     l1 = selectnode(fens; box = Float64[-L/2 -L/2 -Inf Inf -Inf Inf], inflate = tolerance)
     for i in [1,2,3,4,5,6]
         setebc!(dchi, l1, true, i)
@@ -71,10 +74,10 @@ function single_dsg3()
     applyebc!(dchi)
     numberdofs!(dchi);
 
-# Assemble the system matrix
+    # Assemble the system matrix
     K = stiffness(femm, geom0, u0, Rfield0, dchi);
 
-# Midpoint of the free edge
+    # Midpoint of the free edge
     nl = selectnode(fens; box = Float64[L/2 L/2 0 0 -Inf Inf], inflate = tolerance)
     lfemm = FEMMBase(IntegDomain(fes, TriRule(3)))
     # computeforce!(forceout, XYZ, tangents, fe_label) = let
@@ -88,12 +91,12 @@ function single_dsg3()
     fi = ForceIntensity(FFlt[0, 0, -g, 0, 0, 0]);
     F = distribloads(lfemm, geom0, dchi, fi, 3);
     
-# Solve
+    # Solve
     U = K\F
     scattersysvec!(dchi, U[:])
     @show dchi.values[nl, 3],  dchi.values[nl, 3]/analyt_sol*100
 
-# Visualization
+    # Visualization
     scattersysvec!(dchi, (L/4)/maximum(abs.(U)).*U)
     update_rotation_field!(Rfield0, dchi)
     plots = cat(plot_space_box([[0 0 -L/2]; [L/2 L/2 L/2]]),
@@ -102,13 +105,102 @@ function single_dsg3()
         plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
     dims = 1)
     pl = render(plots)
+    return true
 end
 
+
+function single_q4sri()
+    # analytical solution for the vertical deflection and the midpoint of the
+    # free edge 
+    # Parameters:
+    E = 2.0e11;
+    nu = 0.3;
+    L = 1.0
+    # Bathe, Iosilevich, and Chapelle (2000) with a refined mesh of
+    # high-order element MITC16
+
+    # thickness = L/100; 
+    # analyt_sol=-9.3355e-5;
+    # g = 80*0.1^0
+
+    thickness = L/1000; 
+    analyt_sol=-6.3941e-5;
+    g = 80*0.1^3#
+    
+    # Mesh
+    n = 4
+    tolerance = L/n/1000
+    fens, fes = Q4block(L,L,n,n);
+    fens.xyz = xyz3(fens)
+    for i in 1:count(fens)
+        x=fens.xyz[i, 1]-L/2; y=fens.xyz[i, 2]-L/2;
+        fens.xyz[i, :] .= (x, y, x^2 - y^2)
+    end
+
+    mater = MatDeforElastIso(DeforModelRed3D, E, nu)
+    
+    sfes = FESetShellQ4()
+    accepttodelegate(fes, sfes)
+    femm = FEMMShellQ4SRIModule.FEMMShellQ4SRI(IntegDomain(fes, GaussRule(2, 2), thickness), mater)
+    stiffness = FEMMShellQ4SRIModule.stiffness
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # Clamped edge
+    l1 = selectnode(fens; box = Float64[-L/2 -L/2 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in [1,2,3,4,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the system matrix
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    # Midpoint of the free edge
+    nl = selectnode(fens; box = Float64[L/2 L/2 0 0 -Inf Inf], inflate = tolerance)
+    lfemm = FEMMBase(IntegDomain(fes, GaussRule(2, 2)))
+    # computeforce!(forceout, XYZ, tangents, fe_label) = let
+    #     n = cross(tangents[:, 1], tangents[:, 2])
+    #     n = n / norm(n)
+    #     forceout .= 0.0
+    #     forceout[3] = -g * n[3]
+    #     return forceout
+    # end
+    # fi = ForceIntensity(FFlt, 6, computeforce!)
+    fi = ForceIntensity(FFlt[0, 0, -g, 0, 0, 0]);
+    F = distribloads(lfemm, geom0, dchi, fi, 3);
+    
+    # Solve
+    U = K\F
+    scattersysvec!(dchi, U[:])
+    @show dchi.values[nl, 3],  dchi.values[nl, 3]/analyt_sol*100
+
+    # Visualization
+    scattersysvec!(dchi, (L/4)/maximum(abs.(U)).*U)
+    update_rotation_field!(Rfield0, dchi)
+    plots = cat(plot_space_box([[0 0 -L/2]; [L/2 L/2 L/2]]),
+        #plot_nodes(fens),
+        plot_midsurface(fens, fes; x = geom0.values, facecolor = "rgb(12, 12, 123)"),
+        plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
+    dims = 1)
+    pl = render(plots)
+    return true
+end
 
 function allrun()
     println("#####################################################")
     println("# single_dsg3 ")
     single_dsg3()
+    println("#####################################################")
+    println("# single_q4sri ")
+    single_q4sri()
     return true
 end # function allrun
 
