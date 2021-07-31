a7952b2a380f67c69168c8c6c640f8ba88992ca8
Petr Krysl <pkrysl@ucsd.edu>
2021-07-31 11:14:38 -0700

    Raasch

---
+++ examples/shells/deformation/raasch_examples.jl
@@ -0,0 +1,271 @@
+"""
+The geometry consists of a “hook” in the form of a curved strip rigidly clamped
+at one end and loaded with a unit in-plane shear along the width at the other
+end. It has two circular segments that are connected at the tangent point. The
+smaller segment has a mean radius of 0.3556 m (14 inches) and spans 60° from
+the clamped end to the tangent point. The larger segment spans 150° from the
+tangent point to the free end and has a mean radius of 1.1684 m (46 inches).
+The hook is 0.0508 m (2 inches) thick and 0.508 m (20 inches) wide, modeled as
+linear elastic with an elastic modulus of 22.77 MPa (3300 psi) and a Poisson's
+ratio of 0.35. In most tests the shear force is applied through the use of a
+distributing coupling constraint. The coupling constraint provides coupling
+between a reference node on which the load is prescribed and the nodes located
+on the free end. The distributed nodal loads on the free end are equivalent to
+a uniformly distributed load of 8.7563 N/m (0.05 lb/in). In two of the tests an
+equivalent shear force is applied as a distributed shear traction instead.
+
+
+"""
+module raasch_examples
+
+using LinearAlgebra
+using FinEtools
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
+using FinEtoolsFlexStructures.FESetShellQ4Module: FESetShellQ4
+using FinEtoolsFlexStructures.FEMMShellDSG3Module
+using FinEtoolsFlexStructures.FEMMShellDSG3IModule
+using FinEtoolsFlexStructures.FEMMShellCSDSG3Module
+using FinEtoolsFlexStructures.FEMMShellIsoPModule
+using FinEtoolsFlexStructures.FEMMShellT3Module
+using FinEtoolsFlexStructures.FEMMShellQ4SRIModule
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, linear_update_rotation_field!, update_rotation_field!
+using FinEtoolsFlexStructures.VisUtilModule: plot_nodes, plot_midline, render, plot_space_box, plot_midsurface, space_aspectratio, save_to_json
+
+using Infiltrator
+
+function test_dsg3i(input = "raasch_s4_5x36.inp", visualize = true)
+    E = 3300.0;
+    nu = 0.35;
+    thickness  =  2.0;
+    tolerance = thickness/2
+    # analytical solution for the vertical deflection under the load
+    analyt_sol = 5.02;
+    R = 46.0;
+
+    output = import_ABAQUS(input)
+    fens = output["fens"]
+    fes = output["fesets"][1]
+
+    connected = findunconnnodes(fens, fes);
+    fens, new_numbering = compactnodes(fens, connected);
+    fes = renumberconn!(fes, new_numbering);
+
+    fens, fes = Q4toT3(fens, fes)
+
+    # plots = cat(plot_space_box([[0 0 -R/2]; [R/2 R/2 R/2]]),
+    #     plot_nodes(fens),
+    #     plot_midsurface(fens, fes);
+    # dims = 1)
+    # pl = render(plots)
+
+    mater = MatDeforElastIso(DeforModelRed3D, E, nu)
+    
+    formul = FEMMShellDSG3IModule
+    # Report
+    @info "Hemisphere, formulation=$(formul)"
+    @info "Mesh: $input"
+
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = formul.make(IntegDomain(fes, TriRule(1), thickness), mater)
+    stiffness = formul.stiffness
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # Clamped end
+    l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in [1,2,3,4,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the system matrix
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    # Load
+    bfes = meshboundary(fes)
+    l1 = selectelem(fens, bfes, box = [97.9615 97.9615 -16 -16 0 20], inflate = tolerance)
+    lfemm = FEMMBase(IntegDomain(subset(bfes, l1), GaussRule(1, 2)))
+    fi = ForceIntensity(FFlt[0, 0, 0.05, 0, 0, 0]);
+    F = distribloads(lfemm, geom0, dchi, fi, 3);
+    
+    # @infiltrate
+    # Solve
+    U = K\F
+    scattersysvec!(dchi, U[:])
+    nl = selectnode(fens; box = Float64[97.9615 97.9615 -16 -16 0 0], inflate = tolerance)
+    targetu =  dchi.values[nl, 3][1]
+    @info "Target: $(round(targetu, digits=8)),  $(round(targetu/analyt_sol, digits = 4)*100)%"
+
+    # Visualization
+    if !visualize
+        return true
+    end
+    scattersysvec!(dchi, (R/2)/maximum(abs.(U)).*U)
+    update_rotation_field!(Rfield0, dchi)
+    plots = cat(plot_space_box([[0 0 -R]; [R R R]]),
+        plot_nodes(fens),
+        plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
+    dims = 1)
+    pl = render(plots)
+    return true
+end
+
+function test_t6(input = "raasch_s4_1x9.inp", visualize = true)
+    E = 3300.0;
+    nu = 0.35;
+    thickness  =  2.0;
+    tolerance = thickness/2
+    # analytical solution for the vertical deflection under the load
+    analyt_sol = 5.02;
+    R = 46.0;
+
+    output = import_ABAQUS(input)
+    fens = output["fens"]
+    fes = output["fesets"][1]
+
+    connected = findunconnnodes(fens, fes);
+    fens, new_numbering = compactnodes(fens, connected);
+    fes = renumberconn!(fes, new_numbering);
+
+    fens, fes = Q4toT3(fens, fes)
+    fens, fes = T3toT6(fens, fes)
+
+    # plots = cat(plot_space_box([[0 0 -R/2]; [R/2 R/2 R/2]]),
+    #     plot_nodes(fens),
+    #     plot_midsurface(fens, fes);
+    # dims = 1)
+    # pl = render(plots)
+
+    mater = MatDeforElastIso(DeforModelRed3D, E, nu)
+    
+    formul = FEMMShellIsoPModule
+    # Report
+    @info "Hemisphere, formulation=$(formul)"
+    @info "Mesh: $input"
+
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = formul.make(IntegDomain(fes, TriRule(3), thickness), mater)
+    stiffness = formul.stiffness
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # Clamped end
+    l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in [1,2,3,4,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the system matrix
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    # Load
+    bfes = meshboundary(fes)
+    l1 = selectelem(fens, bfes, box = [97.9615 97.9615 -16 -16 0 20], inflate = tolerance)
+    lfemm = FEMMBase(IntegDomain(subset(bfes, l1), GaussRule(1, 2)))
+    fi = ForceIntensity(FFlt[0, 0, 0.05, 0, 0, 0]);
+    F = distribloads(lfemm, geom0, dchi, fi, 3);
+    
+    # @infiltrate
+    # Solve
+    U = K\F
+    scattersysvec!(dchi, U[:])
+    nl = selectnode(fens; box = Float64[97.9615 97.9615 -16 -16 0 0], inflate = tolerance)
+    targetu =  dchi.values[nl, 3][1]
+    @info "Target: $(round(targetu, digits=8)),  $(round(targetu/analyt_sol, digits = 4)*100)%"
+
+    # Visualization
+    if !visualize
+        return true
+    end
+    scattersysvec!(dchi, (R/2)/maximum(abs.(U)).*U)
+    update_rotation_field!(Rfield0, dchi)
+    plots = cat(plot_space_box([[0 0 -R]; [R R R]]),
+        plot_nodes(fens),
+        plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
+    dims = 1)
+    pl = render(plots)
+    return true
+end
+
+function test_dsg3_convergence()
+    for n in [2, 4, 8, 16, 32, 64]
+        test_dsg3(n, false)
+    end
+    return true
+end
+
+function test_dsg3i_convergence()
+    for n in [2, 4, 8, 16, 32, 64]
+        test_dsg3i(n, false)
+    end
+    return true
+end
+
+function test_csdsg3_convergence()
+    for n in [2, 4, 8, 16, 32, 64]
+        test_csdsg3(n, false)
+    end
+    return true
+end
+
+function test_t6_convergence()
+    for n in [2, 4, 8, 16, 32, 64]
+        test_t6(n, false)
+    end
+    return true
+end
+
+function test_q4sri_convergence()
+    for n in [2, 4, 8, 16, 32, 64]
+        test_q4sri(n, false)
+    end
+    return true
+end
+
+function allrun()
+    println("#####################################################")
+    println("# test_dsg3 ")
+    test_dsg3()
+    println("#####################################################")
+    println("# test_q4sri ")
+    test_q4sri()
+    println("#####################################################")
+    println("# test_dsg3_convergence  ")
+    test_dsg3_convergence()
+    println("#####################################################")
+    println("# test_q4sri_convergence  ")
+    test_q4sri_convergence()
+    return true
+end # function allrun
+
+end # module
+
+using .raasch_examples
+raasch_examples.test_dsg3i()
+raasch_examples.test_t6()
+# raasch_examples.test_dsg3()
+# raasch_examples.test_dsg3_convergence()
+# raasch_examples.test_dsg3i_convergence()# 
+# raasch_examples.test_csdsg3_convergence()
+# raasch_examples.test_t6_convergence()
+# raasch_examples.test_q4sri_convergence()
