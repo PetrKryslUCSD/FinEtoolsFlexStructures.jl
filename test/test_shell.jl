9c998cf9c5d93f87f884b02068f049b3e1f24ddd
Petr Krysl <pkrysl@ucsd.edu>
2021-08-04 19:57:03 -0700

    More tests

--- test/test_shell.jl
+++ test/test_shell.jl
@@ -1,5 +1,5 @@
 # Pinched cylinder with diagphram supports and concentrated force
-module scordelis_lo_test
+module scordelis_lo__dsg3_verification
 
 using Test
 using LinearAlgebra
@@ -126,17 +126,165 @@ test_convergence(test_dsg3i, [68.93400706852107,
 97.03680208373054,                                                              
 100.46625686052543,                                                             
 103.64317681989752])
-test_convergence(test_dsg3if, [66.68238024904946,                       
-85.61660951581757,                                                              
-89.90356898669276,                                                              
-92.54697050425355,                                                              
-95.43101599843146,                                                              
-97.66729527571941])
+test_convergence(test_dsg3if, [66.64995973373549,                       
+    85.59194662963257,                                                              
+    89.88220350622672,                                                              
+    92.52849163603474,                                                              
+    95.41700393607626,                                                              
+    97.65867120146895])
 test_convergence(test_csdsg3, [67.6211603156003,                       
     87.47670927679906,                                                              
     92.07919359232324,                                                              
     94.99649878356443,                                                              
     98.38626631439227,                                                              
     101.66676977204703])
+
 end # module
+
+"""
+The geometry consists of a “hook” in the form of a curved strip rigidly clamped
+at one end and loaded with a unit in-plane shear along the width at the other
+end. It has two circular segments that are connected at the tangent point. The
+smaller segment has a mean radius of 0.3556 m (14 inches) and spans 60° from
+the clamped end to the tangent point. The larger segment spans 150° from the
+tangent point to the free end and has a mean radius of 1.1684 m (46 inches).
+The hook is 0.0508 m (2 inches) thick and 0.508 m (20 inches) wide, modeled as
+linear elastic with an elastic modulus of 22.77 MPa (3300 psi) and a Poisson's
+ratio of 0.35. In most tests the shear force is applied through the use of a
+distributing coupling constraint. The coupling constraint provides coupling
+between a reference node on which the load is prescribed and the nodes located
+on the free end. The distributed nodal loads on the free end are equivalent to
+a uniformly distributed load of 8.7563 N/m (0.05 lb/in). In two of the tests an
+equivalent shear force is applied as a distributed shear traction instead.
+
+
+"""
+module raasch_dsg3_verification
+
+using Test
+using LinearAlgebra
+using FinEtools
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
+using FinEtoolsFlexStructures.FESetShellQ4Module: FESetShellQ4
+using FinEtoolsFlexStructures.FEMMShellDSG3Module
+using FinEtoolsFlexStructures.FEMMShellDSG3IModule
+using FinEtoolsFlexStructures.FEMMShellDSG3IFModule
+using FinEtoolsFlexStructures.FEMMShellCSDSG3Module
+using FinEtoolsFlexStructures.FEMMShellIsoPModule
+using FinEtoolsFlexStructures.FEMMShellT3Module
+using FinEtoolsFlexStructures.FEMMShellQ4SRIModule
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, linear_update_rotation_field!, update_rotation_field!
+using FinEtoolsFlexStructures.VisUtilModule: plot_nodes, plot_midline, render, plot_space_box, plot_midsurface, space_aspectratio, save_to_json
+
+using Infiltrator
+
+function test_dsg3if(args...)
+    return _execute_dsg_model(FEMMShellDSG3IFModule, args...)
+end
+
+function test_dsg3i(args...)
+  return _execute_dsg_model(FEMMShellDSG3IModule, args...)
+end
+
+function test_dsg3(args...)
+  return _execute_dsg_model(FEMMShellDSG3Module, args...)
+end
+
+function test_csdsg3(args...)
+  return _execute_dsg_model(FEMMShellCSDSG3Module, args...)
+end
+
+function _execute_dsg_model(formul, input = "raasch_s4_1x9.inp", visualize = true)
+    E = 3300.0;
+    nu = 0.35;
+    thickness  =  2.0;
+    tolerance = thickness/2
+    # analytical solution for the vertical deflection under the load
+    analyt_sol = 5.02;
+    R = 46.0;
+
+    output = import_ABAQUS(joinpath(input))
+    fens = output["fens"]
+    fes = output["fesets"][1]
+
+    connected = findunconnnodes(fens, fes);
+    fens, new_numbering = compactnodes(fens, connected);
+    fes = renumberconn!(fes, new_numbering);
+
+    fens, fes = Q4toT3(fens, fes)
+
+    # plots = cat(plot_space_box([[0 0 -R/2]; [R/2 R/2 R/2]]),
+    #     plot_nodes(fens),
+    #     plot_midsurface(fens, fes);
+    # dims = 1)
+    # pl = render(plots)
+
+    mater = MatDeforElastIso(DeforModelRed3D, E, nu)
+    
+        # Report
+    # @info "Raasch hook, formulation=$(formul)"
+    # @info "Mesh: $input"
+
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = formul.make(IntegDomain(fes, TriRule(1), thickness), mater)
+    stiffness = formul.stiffness
+    associategeometry! = formul.associategeometry!
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # Clamped end
+    l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in [1,2,3,4,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the system matrix
+    associategeometry!(femm, geom0)
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    # Load
+    bfes = meshboundary(fes)
+    l1 = selectelem(fens, bfes, box = [97.9615 97.9615 -16 -16 0 20], inflate = tolerance)
+    lfemm = FEMMBase(IntegDomain(subset(bfes, l1), GaussRule(1, 2)))
+    fi = ForceIntensity(FFlt[0, 0, 0.05, 0, 0, 0]);
+    F = distribloads(lfemm, geom0, dchi, fi, 3);
+    
+    # @infiltrate
+    # Solve
+    U = K\F
+    scattersysvec!(dchi, U[:])
+    nl = selectnode(fens; box = Float64[97.9615 97.9615 -16 -16 0 0], inflate = tolerance)
+    targetu =  dchi.values[nl, 3][1]
+    # @info "Target: $(round(targetu, digits=8)),  $(round(targetu/analyt_sol, digits = 4)*100)%"
+
+    return targetu/analyt_sol*100
+end
+ 
+function test_convergence(t, results)
+    for (m, res) in zip(["1x9", "3x18", "5x36", "10x72"], results)
+        v = t("raasch_s4_" * m * ".inp", false)
+        @test res ≈ v
+    end
+    return true
+end
+
+ 
+test_convergence(test_dsg3, [197.07225021939522, 122.29375814892506, 131.7397746827515, 166.9855842073799])
+test_convergence(test_dsg3i, [98.93041135486558, 107.56727708008175, 121.0279541750594, 160.55183716024845])
+test_convergence(test_dsg3if, [96.51445461618886, 96.48046436369553, 97.30091092942541, 98.41519147885221])
+test_convergence(test_csdsg3, [90.09510193432982, 101.2652927601732, 114.4122860334214, 155.13736181762695])
+       
+end # module
+
  
