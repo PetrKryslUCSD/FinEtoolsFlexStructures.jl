e18fceeebe3d8dfe9159d32cd64b3a37486e31cc
Petr Krysl <pkrysl@ucsd.edu>
2021-08-04 20:30:15 -0700

    Twisted beam tests

--- test/test_shell.jl
+++ test/test_shell.jl
@@ -287,4 +287,169 @@ test_convergence(test_csdsg3, [90.09510193432982, 101.2652927601732, 114.4122860
        
 end # module
 
+"""
+The initially twisted cantilever beam is one of the standard test
+problems for verifying the finite-element accuracy [1]. The beam is
+clamped at one end and loaded either with unit in-plane or 
+unit out-of-plane force at the other. The centroidal axis of the beam is
+straight at the undeformed  configuration, while its cross-sections are
+twisted about the centroidal axis from 0 at the clamped end to pi/2 at
+the free end. 
+
+Reference:
+Zupan D, Saje M (2004) On "A proposed standard set of problems to test
+finite element accuracy": the twisted beam. Finite Elements in Analysis
+and Design 40: 1445-1451.  
+
+Thicker cross section (t = 0.32)
+#     Loading in the Z direction
+dir = 3; uex = 0.005424534868469; # Harder: 5.424e-3;
+#     Loading in the Y direction
+dir = 2; uex = 0.001753248285256; # Harder: 1.754e-3;
+
+Thinner cross section (t = 0.0032)
+#     Loading in the Z direction
+dir = 3; uex = 0.005256;
+#     Loading in the Y direction
+dir = 2; uex = 0.001294;
+
+MacNeal,  R. H., and R. L. Harder, “A Proposed Standard Set of Problems to Test Finite Element Accuracy,” Finite Elements in Analysis Design, vol. 11, pp. 3–20, 1985.
+
+Simo,  J. C., D. D. Fox, and M. S. Rifai, “On a Stress Resultant Geometrically Exact Shell Model. Part II: The Linear Theory; Computational Aspects,” Computational Methods in Applied Mechanical Engineering, vol. 73, pp. 53–92, 1989.
+
+
+"""
+module twisted_beam_examples
+
+using Test
+using FinEtools
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
+using FinEtoolsFlexStructures.FESetShellQ4Module: FESetShellQ4
+using FinEtoolsFlexStructures.FEMMShellDSG3Module
+using FinEtoolsFlexStructures.FEMMShellDSG3IModule
+using FinEtoolsFlexStructures.FEMMShellDSG3IFModule
+using FinEtoolsFlexStructures.FEMMShellCSDSG3Module
+using FinEtoolsFlexStructures.FEMMShellIsoPModule
+using FinEtoolsFlexStructures.FEMMShellQ4SRIModule
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, linear_update_rotation_field!, update_rotation_field!
+using FinEtoolsFlexStructures.VisUtilModule: plot_nodes, plot_midline, render, plot_space_box, plot_midsurface, space_aspectratio, save_to_json, plot_triads
+
+params_thicker_dir_3 = (t =  0.32, force = 1.0, dir = 3, uex = 0.005424534868469); 
+params_thicker_dir_2 = (t =  0.32, force = 1.0, dir = 2, uex = 0.001753248285256); 
+
+params_thinner_dir_3 = (t =  0.0032, force = 1.0e-6, dir = 3, uex = 0.005256); 
+params_thinner_dir_2 = (t =  0.0032, force = 1.0e-6, dir = 2, uex = 0.001294); 
+
+
+function test_dsg3if(args...)
+    return _execute_dsg_model(FEMMShellDSG3IFModule, args...)
+end
+
+function test_dsg3i(args...)
+  return _execute_dsg_model(FEMMShellDSG3IModule, args...)
+end
+
+function test_dsg3(args...)
+  return _execute_dsg_model(FEMMShellDSG3Module, args...)
+end
+
+function _execute_dsg_model(formul, t = 0.32, force = 1.0, dir = 3, uex = 0.005424534868469, nL = 8, nW = 2, visualize = true)
+    E = 0.29e8;
+    nu = 0.22;
+    W = 1.1;
+    L = 12.0;
+    
+    tolerance = W/nW/100
+    fens, fes = T3block(L,W,nL,nW,:a);
+    fens.xyz = xyz3(fens)
+    for i in 1:count(fens)
+        a=fens.xyz[i,1]/L*(pi/2); y=fens.xyz[i,2]-(W/2); z=fens.xyz[i,3];
+        fens.xyz[i,:]=[fens.xyz[i,1],y*cos(a)-z*sin(a),y*sin(a)+z*cos(a)];
+    end
+    
+    mater = MatDeforElastIso(DeforModelRed3D, E, nu)
+    
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = formul.make(IntegDomain(fes, TriRule(1), t), mater)
+    stiffness = formul.stiffness
+    associategeometry! = formul.associategeometry!
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # Clamped end
+    l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in 1:6
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the system matrix
+    associategeometry!(femm, geom0)
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    # Load
+    nl = selectnode(fens; box = Float64[L L 0 0 0 0], tolerance = tolerance)
+    loadbdry = FESetP1(reshape(nl, 1, 1))
+    lfemm = FEMMBase(IntegDomain(loadbdry, PointRule()))
+    v = FFlt[0, 0, 0, 0, 0, 0]
+    v[dir] = force
+    fi = ForceIntensity(v);
+    F = distribloads(lfemm, geom0, dchi, fi, 3);
+
+    # Solve
+    U = K\F
+    scattersysvec!(dchi, U[:])
+    result =  dchi.values[nl, dir][1]/uex*100
+return result
+end
+
+function test_convergence(t, results)
+    for n in [2, 4, 8, 16, ]
+        v = t(params_thicker_dir_2..., 2*n, n, false)
+        @test v ≈ popat!(results, 1)
+    end
+    for n in [2, 4, 8, 16, ]
+        v = t(params_thicker_dir_3..., 2*n, n, false)
+        @test v ≈ popat!(results, 1)
+    end
+    for n in [2, 4, 8, 16, ]
+        v = t(params_thinner_dir_2..., 2*n, n, false)
+        @test v ≈ popat!(results, 1)
+    end
+    for n in [2, 4, 8, 16, ]
+        v = t(params_thinner_dir_3..., 2*n, n, false)
+        @test v ≈ popat!(results, 1)
+    end
+    return true
+end
+
+test_convergence(test_dsg3if, [54.64381168194862                                  
+73.44131219368768                                  
+87.32404900158092                                  
+95.46324216331342                                  
+79.62634140519276                                  
+91.14293575791413                                  
+96.58985595218654                                  
+98.66116466318049                                  
+68.78326551886569                                  
+85.06734683656741                                  
+92.79623471964817                                  
+96.50986488444092                                  
+79.23331115523263                                  
+88.44430639472728                                  
+93.60167377948054                                  
+96.63868822689857 ])
+
+end # module
+
  
+
