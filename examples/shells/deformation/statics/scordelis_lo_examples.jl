81c40e856a77d0f8ad19a5b0242b7ab122f03ebb
Petr Krysl <pkrysl@ucsd.edu>
2021-08-05 17:07:19 -0700

    sco5lo

---
+++ examples/shells/deformation/statics/scordelis_lo_examples.jl
@@ -0,0 +1,320 @@
+# Pinched cylinder with diagphram supports and concentrated force
+module scordelis_lo_examples
+
+using LinearAlgebra
+using FinEtools
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
+using FinEtoolsFlexStructures.FESetShellQ4Module: FESetShellQ4
+using FinEtoolsFlexStructures.FEMMShellDSG3Module
+using FinEtoolsFlexStructures.FEMMShellDSG3IFModule
+using FinEtoolsFlexStructures.FEMMShellDSG3IModule
+using FinEtoolsFlexStructures.FEMMShellCSDSG3Module
+using FinEtoolsFlexStructures.FEMMShellIsoPModule
+using FinEtoolsFlexStructures.FEMMShellQ4SRIModule
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, linear_update_rotation_field!, update_rotation_field!
+using FinEtoolsFlexStructures.VisUtilModule: plot_nodes, plot_midline, render, plot_space_box, plot_midsurface, space_aspectratio, save_to_json
+
+using Infiltrator
+
+function test_dsg3if(args...)
+    return _execute_dsg_model(FEMMShellDSG3IFModule, args...)
+end
+
+function test_dsg3i(args...)
+  return _execute_dsg_model(FEMMShellDSG3IModule, args...)
+end
+
+function test_dsg3(args...)
+  return _execute_dsg_model(FEMMShellDSG3Module, args...)
+end
+
+function test_csdsg3(args...)
+  return _execute_dsg_model(FEMMShellCSDSG3Module, args...)
+end
+
+function _execute_dsg_model(formul, n = 8, visualize = true)
+    # analytical solution for the vertical deflection and the midpoint of the
+    # free edge 
+    analyt_sol=-0.3024;
+    # Parameters:
+    E=4.32e8;
+    nu=0.0;
+    thickness = 0.25; # geometrical dimensions are in feet
+    R = 25.0;
+    L = 50.0;
+    
+    tolerance = R/n/1000
+    fens, fes = T3block(40/360*2*pi,L/2,n,n);
+    fens.xyz = xyz3(fens)
+    for i in 1:count(fens)
+        a=fens.xyz[i, 1]; y=fens.xyz[i, 2];
+        fens.xyz[i, :] .= (R*sin(a), y, R*(cos(a)-1))
+    end
+
+    mater = MatDeforElastIso(DeforModelRed3D, E, nu)
+    
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = formul.make(IntegDomain(fes, TriRule(1), thickness), mater)
+    stiffness = formul.stiffness
+    associategeometry! = formul.associategeometry!
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # rigid diaphragm
+    l1 = selectnode(fens; box = Float64[-Inf Inf 0 0 -Inf Inf], inflate = tolerance)
+    for i in [1,3,5]
+        setebc!(dchi, l1, true, i)
+    end
+    # plane of symmetry perpendicular to Y
+    l1 = selectnode(fens; box = Float64[-Inf Inf L/2 L/2 -Inf Inf], inflate = tolerance)
+    for i in [2,4,6]
+        setebc!(dchi, l1, true, i)
+    end
+    # plane of symmetry perpendicular to X
+    l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in [1,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the system matrix
+    associategeometry!(femm, geom0)
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    # Midpoint of the free edge
+    nl = selectnode(fens; box = Float64[sin(40/360*2*pi)*25 sin(40/360*2*pi)*25 L/2 L/2 -Inf Inf], inflate = tolerance)
+    lfemm = FEMMBase(IntegDomain(fes, TriRule(3)))
+    fi = ForceIntensity(FFlt[0, 0, -90, 0, 0, 0]);
+    F = distribloads(lfemm, geom0, dchi, fi, 3);
+    
+    # Solve
+    U = K\F
+    scattersysvec!(dchi, U[:])
+    resultpercent =   dchi.values[nl, 3][1]/analyt_sol*100
+    @info "Solution: $(round(resultpercent, digits = 4))%"
+
+    # Visualization
+    if visualize
+        scattersysvec!(dchi, (L/8)/maximum(abs.(U)).*U)
+        update_rotation_field!(Rfield0, dchi)
+        plots = cat(plot_space_box([[0 0 -L/2]; [L/2 L/2 L/2]]),
+            #plot_nodes(fens),
+            plot_midsurface(fens, fes; x = geom0.values, facecolor = "rgb(12, 12, 123)"),
+            plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
+            dims = 1)
+        pl = render(plots)
+    end
+
+    return true
+end
+
+function test_q4sri(n = 8, visualize = true)
+    # analytical solution for the vertical deflection and the midpoint of the
+    # free edge 
+    analyt_sol=-0.3024;
+    # Parameters:
+    E=4.32e8;
+    nu=0.0;
+    thickness = 0.25; # geometrical dimensions are in feet
+    R = 25.0;
+    L = 50.0;
+    
+    tolerance = R/n/1000
+    fens, fes = Q4block(40/360*2*pi,L/2,n,n);
+    fens.xyz = xyz3(fens)
+    for i in 1:count(fens)
+        a=fens.xyz[i, 1]; y=fens.xyz[i, 2];
+        fens.xyz[i, :] .= (R*sin(a), y, R*(cos(a)-1))
+    end
+
+    mater = MatDeforElastIso(DeforModelRed3D, E, nu)
+    
+    sfes = FESetShellQ4()
+    accepttodelegate(fes, sfes)
+    femm = FEMMShellQ4SRIModule.FEMMShellQ4SRI(IntegDomain(fes, GaussRule(2, 2), thickness), mater)
+    stiffness = FEMMShellQ4SRIModule.stiffness
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # rigid diaphragm
+    l1 = selectnode(fens; box = Float64[-Inf Inf 0 0 -Inf Inf], inflate = tolerance)
+    for i in [1,3,5]
+        setebc!(dchi, l1, true, i)
+    end
+    # plane of symmetry perpendicular to Y
+    l1 = selectnode(fens; box = Float64[-Inf Inf L/2 L/2 -Inf Inf], inflate = tolerance)
+    for i in [2,4,6]
+        setebc!(dchi, l1, true, i)
+    end
+    # plane of symmetry perpendicular to X
+    l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in [1,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+    @show dchi.nfreedofs
+
+    # Assemble the system matrix
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    # Midpoint of the free edge
+    nl = selectnode(fens; box = Float64[sin(40/360*2*pi)*25 sin(40/360*2*pi)*25 L/2 L/2 -Inf Inf], inflate = tolerance)
+    lfemm = FEMMBase(IntegDomain(fes, GaussRule(2, 2)))
+    fi = ForceIntensity(FFlt[0, 0, -90, 0, 0, 0]);
+    F = distribloads(lfemm, geom0, dchi, fi, 3);
+    
+    # Solve
+    U = K\F
+    scattersysvec!(dchi, U[:])
+    @show dchi.values[nl, 3],  dchi.values[nl, 3]/analyt_sol*100
+
+    # Visualization
+    if visualize
+        scattersysvec!(dchi, (L/8)/maximum(abs.(U)).*U)
+        update_rotation_field!(Rfield0, dchi)
+        plots = cat(plot_space_box([[0 0 -L/2]; [L/2 L/2 L/2]]),
+            #plot_nodes(fens),
+            plot_midsurface(fens, fes; x = geom0.values, facecolor = "rgb(12, 12, 123)"),
+            plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
+            dims = 1)
+        pl = render(plots)
+    end
+
+    return true
+end
+
+
+function test_t6(n = 8, visualize = true)
+    # analytical solution for the vertical deflection and the midpoint of the
+    # free edge 
+    analyt_sol=-0.3024;
+    # Parameters:
+    E=4.32e8;
+    nu=0.0;
+    thickness = 0.25; # geometrical dimensions are in feet
+    R = 25.0;
+    L = 50.0;
+    
+    tolerance = R/n/1000
+    fens, fes = T6block(40/360*2*pi,L/2,n,n);
+    fens.xyz = xyz3(fens)
+    for i in 1:count(fens)
+        a=fens.xyz[i, 1]; y=fens.xyz[i, 2];
+        fens.xyz[i, :] .= (R*sin(a), y, R*(cos(a)-1))
+    end
+
+    mater = MatDeforElastIso(DeforModelRed3D, E, nu)
+    
+    formul = FEMMShellIsoPModule
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = formul.make(IntegDomain(fes, TriRule(6), thickness), mater)
+    stiffness = formul.stiffness
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # rigid diaphragm
+    l1 = selectnode(fens; box = Float64[-Inf Inf 0 0 -Inf Inf], inflate = tolerance)
+    for i in [1,3,5]
+        setebc!(dchi, l1, true, i)
+    end
+    # plane of symmetry perpendicular to Y
+    l1 = selectnode(fens; box = Float64[-Inf Inf L/2 L/2 -Inf Inf], inflate = tolerance)
+    for i in [2,4,6]
+        setebc!(dchi, l1, true, i)
+    end
+    # plane of symmetry perpendicular to X
+    l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in [1,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the system matrix
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    # Midpoint of the free edge
+    nl = selectnode(fens; box = Float64[sin(40/360*2*pi)*25 sin(40/360*2*pi)*25 L/2 L/2 -Inf Inf], inflate = tolerance)
+    lfemm = FEMMBase(IntegDomain(fes, TriRule(3)))
+    fi = ForceIntensity(FFlt[0, 0, -90, 0, 0, 0]);
+    F = distribloads(lfemm, geom0, dchi, fi, 3);
+    
+    # Solve
+    U = K\F
+    scattersysvec!(dchi, U[:])
+    @show dchi.values[nl, 3]/analyt_sol*100
+
+    # Visualization
+    if visualize
+        scattersysvec!(dchi, (L/8)/maximum(abs.(U)).*U)
+        update_rotation_field!(Rfield0, dchi)
+        plots = cat(plot_space_box([[0 0 -L/2]; [L/2 L/2 L/2]]),
+        #plot_nodes(fens),
+            plot_midsurface(fens, fes; x = geom0.values, facecolor = "rgb(12, 12, 123)"),
+            plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
+            dims = 1)
+        pl = render(plots)
+    end
+
+    return true
+end
+
+function test_convergence(t)
+    @info "Scordelis-Lo shell, formulation=$(t)"
+    for n in [4, 8, 10, 12, 16, 24]
+        t(n, false)
+    end
+    return true
+end
+
+
+function allrun()
+    println("#####################################################")
+    println("# test_dsg3 ")
+    test_dsg3()
+    println("#####################################################")
+    println("# test_q4sri ")
+    test_q4sri()
+    println("#####################################################")
+    println("# test_dsg3_convergence  ")
+    test_dsg3_convergence()
+    println("#####################################################")
+    println("# test_csdsg3_convergence  ")
+    test_csdsg3_convergence()
+    println("#####################################################")
+    println("# test_q4sri_convergence  ")
+    test_q4sri_convergence()
+    return true
+end # function allrun
+
+end # module
+
+using .scordelis_lo_examples
+m = scordelis_lo_examples
+m.test_convergence(m.test_dsg3)
+m.test_convergence(m.test_dsg3i)
+m.test_convergence(m.test_dsg3if)
+m.test_convergence(m.test_csdsg3)
