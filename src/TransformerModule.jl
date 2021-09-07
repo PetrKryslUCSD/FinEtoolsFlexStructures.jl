43c174f064d12fc2d0e71f056e370e6f848530be
Petr Krysl <pkrysl@ucsd.edu>
2021-09-07 13:42:13 -0700

    transformer

---
+++ src/TransformerModule.jl
@@ -0,0 +1,22 @@
+module TransformerModule
+
+using LinearAlgebra: norm, Transpose, mul!
+using FinEtools
+
+struct Transformer
+    _buff::FFltMat
+    function Transformer(T)
+        _buff = fill(0.0, size(T)...); 
+        return new(_buff)
+    end
+end
+
+(o::Transformer)(elmat, T) = begin
+    @assert size(o._buff) == size(T)
+    @assert size(elmat) == size(T)
+    mul!(o._buff, elmat, Transpose(T))
+    mul!(elmat, T, o._buff)
+    return elmat
+end
+
+end # module
