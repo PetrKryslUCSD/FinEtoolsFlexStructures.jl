5d077990bb1666d85194a5dc4a6a504f98512f01
Petr Krysl <pkrysl@ucsd.edu>
2021-09-19 15:30:46 -0700

    output resultants

---
+++ examples/shells/deformation/statics/simply_supp_square_plate_conc_resultant_examples.jl
@@ -0,0 +1,121 @@
+# Simply supported square plate with center concentrated load
+module simply_supp_square_plate_conc_resultant_examples
+
+using FinEtools
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
+using FinEtoolsFlexStructures.FESetShellQ4Module: FESetShellQ4
+using FinEtoolsFlexStructures.FEMMShellT3DSGAModule
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, linear_update_rotation_field!, update_rotation_field!
+using FinEtoolsFlexStructures.VisUtilModule: plot_nodes, plot_midline, render, plot_space_box, plot_midsurface, space_aspectratio, save_to_json
+using FinEtools.MeshExportModule.VTKWrite: vtkwrite
+
+function _execute_dsg_model(formul, n = 2, visualize = true)
+    E = 30e6;
+    nu = 0.3;
+    force = 40;
+    thickness = 0.1;
+    L= 10;
+    # analytical solution for the vertical deflection under the load
+    analyt_sol=-0.0168;
+
+    tolerance = L/n/1000
+    fens, fes = T3block(L/2,L/2,n,n);
+    fens.xyz = xyz3(fens)
+
+    mater = MatDeforElastIso(DeforModelRed3D, E, nu)
+    
+        sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = formul.make(IntegDomain(fes, TriRule(1), thickness), mater)
+    stiffness = formul.stiffness
+    associategeometry! = formul.associategeometry!
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # plane of symmetry perpendicular to X
+    l1 = selectnode(fens; box = Float64[0 0 0 L/2 -Inf Inf], inflate = tolerance)
+    for i in [1,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    # plane of symmetry perpendicular to Y
+    l1 = selectnode(fens; box = Float64[0 L/2 0 0 -Inf Inf], inflate = tolerance)
+    for i in [2,4,6]
+        setebc!(dchi, l1, true, i)
+    end
+    # simple support
+    l1 = selectnode(fens; box = Float64[L/2 L/2 0 L/2 -Inf Inf], inflate = tolerance)
+    for i in [3,4,6]
+        setebc!(dchi, l1, true, i)
+    end
+    l1 = selectnode(fens; box = Float64[0 L/2 L/2 L/2 -Inf Inf], inflate = tolerance)
+    for i in [3,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    # in-plane, rotations
+    l1 = selectnode(fens; box = Float64[0 L/2 0 L/2 -Inf Inf], inflate = tolerance)
+    for i in [1, 2, 6]
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the system matrix
+    associategeometry!(femm, geom0)
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    # Load
+    nl = selectnode(fens; box = Float64[0 0 0 0 -Inf Inf], tolerance = tolerance)
+    loadbdry = FESetP1(reshape(nl, 1, 1))
+    lfemm = FEMMBase(IntegDomain(loadbdry, PointRule()))
+    fi = ForceIntensity(FFlt[0, 0, -force/4, 0, 0, 0]);
+    F = distribloads(lfemm, geom0, dchi, fi, 3);
+
+    # Solve
+    U = K\F
+    scattersysvec!(dchi, U[:])
+    @show dchi.values[nl, 3]/analyt_sol*100
+
+    # Generate a graphical display of resultants
+    # fld= fieldfromintegpoints(femm, geom0, dchi, :moment, 1)
+    fld= elemfieldfromintegpoints(femm, geom0, dchi, :moment, 1)
+    vtkwrite("plate-m1.vtu", fens, fes; scalars = [("m1", fld.values)])
+
+    # Visualization
+    if !visualize
+        return true
+    end
+    scattersysvec!(dchi, (L/4)/maximum(abs.(U)).*U)
+    update_rotation_field!(Rfield0, dchi)
+    plots = cat(plot_space_box([[0 0 -L/2]; [L/2 L/2 L/2]]),
+        #plot_nodes(fens),
+        plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
+    dims = 1)
+    pl = render(plots)
+
+
+    return true
+end
+
+function test_convergence()
+    formul = FEMMShellT3DSGAModule
+    @info "Simply supported square plated with concentrated force,"
+    @info " formulation=$(formul)"
+
+    for n in [12, ]
+        _execute_dsg_model(formul, n, true)
+    end
+    return true
+end
+
+end # module
+
+using .simply_supp_square_plate_conc_resultant_examples
+m = simply_supp_square_plate_conc_resultant_examples
+m.test_convergence()
