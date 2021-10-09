c4057a4100f1c0bd3f63a949ded2078cacb72869
Petr Krysl <pkrysl@ucsd.edu>
2021-10-09 10:05:28 -0700

    And export graphics

--- examples/shells/deformation/statics/raasch_examples.jl
+++ examples/shells/deformation/statics/raasch_examples.jl
@@ -18,7 +18,7 @@ equivalent shear force is applied as a distributed shear traction instead.
 """
 module raasch_examples
 
-using LinearAlgebra
+using LinearAlgebra, Statistics
 using FinEtools
 using FinEtoolsDeforLinear
 using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
@@ -30,25 +30,50 @@ using FinEtools.MeshExportModule.VTKWrite: vtkwrite
 
 using Infiltrator
 
-function _execute(input = "raasch_s4_1x9.inp", visualize = true)
+function _execute(input = "raasch_s4_1x9.inp", drilling_stiffness_scale = 1.0, visualize = true, nL = 9, nW = 1)
     E = 3300.0;
     nu = 0.35;
     thickness  =  2.0;
     tolerance = thickness/2
     # analytical solution for the vertical deflection under the load
-    analyt_sol = 5.02;
+    analyt_sol = 5.09;
     R = 46.0;
     formul = FEMMShellT3FFModule
+    # formul = FEMMShellT3DSGMTModule
+    @show formul
 
-    output = import_ABAQUS(joinpath(dirname(@__FILE__()), input))
-    fens = output["fens"]
-    fes = output["fesets"][1]
+    if input == ""
+        fens, fes = T3block(210.0, 20.0, nL, nW)
+        fens.xyz = xyz3(fens)
+        for k in 1:count(fens)
+            a, z = fens.xyz[k, :]
+            if a <= 60.0
+                fens.xyz[k, :] .= (14*sin(a*pi/180), 14*(1-cos(a*pi/180)), z)
+            else
+                a = a - 60.0 + 30.0
+                xc = 14*sin(60*pi/180) + 46*cos(30*pi/180)
+                yc = 14*(1-cos(60*pi/180)) - 46*sin(30*pi/180)
+                fens.xyz[k, :] .= (xc-46*cos(a*pi/180), yc+46*sin(a*pi/180), z)
+            end
+        end
+        input = "raasch_$(nL)x$(nW)"
+    else
+        output = import_ABAQUS(joinpath(dirname(@__FILE__()), input))
+        fens = output["fens"]
+        fes = output["fesets"][1]
 
-    connected = findunconnnodes(fens, fes);
-    fens, new_numbering = compactnodes(fens, connected);
-    fes = renumberconn!(fes, new_numbering);
+        connected = findunconnnodes(fens, fes);
+        fens, new_numbering = compactnodes(fens, connected);
+        fes = renumberconn!(fes, new_numbering);
 
-    fens, fes = Q4toT3(fens, fes)
+        fens, fes = Q4toT3(fens, fes)
+
+        # fens, fes = T3refine(fens, fes)# .
+        # fens, fes = T3refine(fens, fes)# .
+        # fens, fes = T3refine(fens, fes)# .
+    end
+
+    @show count(fens), count(fes)
 
     # plots = cat(plot_space_box([[0 0 -R/2]; [R/2 R/2 R/2]]),
     #     plot_nodes(fens),
@@ -65,7 +90,7 @@ function _execute(input = "raasch_s4_1x9.inp", visualize = true)
     sfes = FESetShellT3()
     accepttodelegate(fes, sfes)
     femm = formul.make(IntegDomain(fes, TriRule(1), thickness), mater)
-    femm.drilling_stiffness_scale = 1.0e-4
+    femm.drilling_stiffness_scale = drilling_stiffness_scale
     stiffness = formul.stiffness
     associategeometry! = formul.associategeometry!
 
@@ -95,53 +120,68 @@ function _execute(input = "raasch_s4_1x9.inp", visualize = true)
     l1 = selectelem(fens, bfes, box = [97.9615 97.9615 -16 -16 0 20], inflate = tolerance)
     lfemm = FEMMBase(IntegDomain(subset(bfes, l1), GaussRule(1, 2)))
     fi = ForceIntensity(FFlt[0, 0, 0.05, 0, 0, 0]);
-    F = distribloads(lfemm, geom0, dchi, fi, 1);
+    F = distribloads(lfemm, geom0, dchi, fi, 3);
     
     # @infiltrate
     # Solve
     U = K\F
     scattersysvec!(dchi, U[:])
-    # @show minimum(dchi.values[:, 1]), maximum(dchi.values[:, 1])
-    # @show minimum(dchi.values[:, 2]), maximum(dchi.values[:, 2])
-    # @show minimum(dchi.values[:, 3]), maximum(dchi.values[:, 3])
-    # @show minimum(dchi.values[:, 4]), maximum(dchi.values[:, 4])
-    # @show minimum(dchi.values[:, 5]), maximum(dchi.values[:, 5])
-    # @show minimum(dchi.values[:, 6]), maximum(dchi.values[:, 6])
+    nl = selectnode(fens; box = Float64[97.9615 97.9615 -16 -16 0 20], inflate = tolerance)
+    targetu =  mean(dchi.values[nl, 3])
+    @info "Solution: $(round(targetu, digits=8)),  $(round(targetu/analyt_sol, digits = 4)*100)%"
 
     # Generate a graphical display of displacements and rotations
     scalars = []
     for nc in 1:6
-        push!(scalars, ("dchi$nc", dchi.values[:, nc]))
+        push!(scalars, ("dchi$nc", deepcopy(dchi.values[:, nc])))
     end
-    vtkwrite("raasch-$input-dchi.vtu", fens, fes; scalars = scalars)
-
-    nl = selectnode(fens; box = Float64[97.9615 97.9615 -16 -16 0 0], inflate = tolerance)
-    targetu =  dchi.values[nl, 3][1]
-    @info "Solution: $(round(targetu, digits=8)),  $(round(targetu/analyt_sol, digits = 4)*100)%"
+    vectors = []
+    push!(vectors, ("U", deepcopy(dchi.values[:, 1:3])))
+    push!(vectors, ("UR", deepcopy(dchi.values[:, 4:6])))
+    vtkwrite("raasch-$input-dchi.vtu", fens, fes; scalars = scalars, vectors = vectors)
 
     # Visualization
-    if !visualize
-        return true
+    if visualize
+        scattersysvec!(dchi, (R/2)/maximum(abs.(U)).*U)
+        update_rotation_field!(Rfield0, dchi)
+        plots = cat(plot_space_box([[0 0 -R]; [R R R]]),
+            plot_nodes(fens),
+            plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
+            dims = 1)
+        pl = render(plots)
     end
-    scattersysvec!(dchi, (R/2)/maximum(abs.(U)).*U)
-    update_rotation_field!(Rfield0, dchi)
-    plots = cat(plot_space_box([[0 0 -R]; [R R R]]),
-        plot_nodes(fens),
-        plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
-    dims = 1)
-    pl = render(plots)
-    return true
+    return targetu/analyt_sol
 end
 
 function test_convergence()
     
     @info "Raasch hook"
 
-    for m in ["1x9", "3x18", "5x36", "10x72", "20x144"]
-    # for m in ["1x9", ]
-        _execute("raasch_s4_" * m * ".inp", false)
+    # for m in ["1x9", "3x18", "5x36", "10x72", "20x144"]
+        # _execute("raasch_s4_" * m * ".inp", 1.0, false)
+    for n in 1:7
+        _execute("", 1.0, false, 9*2^(n-1), 1*2^(n-1))
     end
     return true
+end
+
+function test_dep_drilling_stiffness_scale()
+    
+    @info "Raasch hook"
+
+    all_results = []
+    all_drilling_stiffness_scale = [1000.0, 1.0, 0.1, 0.0001, 0.000001] 
+    for drilling_stiffness_scale in all_drilling_stiffness_scale
+        results = Float64[]
+        # for m in ["1x9", "3x18", "5x36", "10x72", "20x144"]
+            # v = _execute("raasch_s4_" * m * ".inp", drilling_stiffness_scale, false)
+        for n in 1:9
+            v = _execute("", drilling_stiffness_scale, false, 9*2^(n-1), 1*2^(n-1))
+                    push!(results, v)
+        end
+        push!(all_results, results)
+    end
+    return all_drilling_stiffness_scale, all_results
 end
 
 end # module
