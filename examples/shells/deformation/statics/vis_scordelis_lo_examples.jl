efc83110670baa6ec1abe1af1b1ef925b9591c40
Petr Krysl <Petr.Krysl.San.Diego@Gmail.com>
2021-12-05 18:19:54 -0800

    visualize the cylindrical roof

---
+++ examples/shells/deformation/statics/vis_scordelis_lo_examples.jl
@@ -0,0 +1,180 @@
+"""
+The barrel vault (Scordelis-Lo) roof is one of the benchmarks for linear elastic
+analysis of shells. 
+
+The candidate element's usefulness in irregular geometries (and most practical
+cases involve a high degree of geometric irregularity) is tested. As would be
+expected,the irregular mesh results are not as good as those provided by a
+regular meshwith the same number of variables. 
+
+Problem description
+
+The physical basis of the problem is a deeply arched roof supported only
+bydiaphragms at its curved edges (an aircraft hanger), deforming under its own
+weight. It is interesting to observe that the geometry is such that the
+centerpoint of the roof moves upward under the self-weight(downwardly directed)
+load. Perhaps this is one reason why the problem is not straightforward
+numerically. 
+"""
+module scordelis_lo_examples
+
+using LinearAlgebra
+using FinEtools
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
+using FinEtoolsFlexStructures.FESetShellQ4Module: FESetShellQ4
+using FinEtoolsFlexStructures.FEMMShellT3FFModule
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, linear_update_rotation_field!, update_rotation_field!
+using FinEtoolsFlexStructures.VisUtilModule: plot_nodes, plot_midline, render, plot_space_box, plot_midsurface, space_aspectratio, save_to_json
+using FinEtools.MeshExportModule.VTKWrite: vtkwrite
+
+# analytical solution for the vertical deflection and the midpoint of the
+# free edge 
+analyt_sol=-0.3024;
+# Parameters:
+E=4.32e8;
+nu=0.0;
+thickness = 0.25; # geometrical dimensions are in feet
+R = 25.0;
+L = 50.0;
+
+cylindrical!(csmatout::FFltMat, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt) = begin
+    r = vec(XYZ); r[2] = 0.0; r[3] += R
+    csmatout[:, 3] .= vec(r)/norm(vec(r))
+    csmatout[:, 2] .= (0.0, 1.0, 0.0) #  this is along the axis
+    cross3!(view(csmatout, :, 1), view(csmatout, :, 2), view(csmatout, :, 3))
+    return csmatout
+end
+
+function _execute_dsg_model(formul, n = 8, visualize = true)
+    
+    
+    tolerance = R/n/10
+    # fens, fes = T3blockrand(40/360*2*pi,L/2,n,n);
+    fens, fes = T3block(40/360*2*pi,L/2,n,n,:b);
+    fens.xyz = xyz3(fens)
+    for i in 1:count(fens)
+        a=fens.xyz[i, 1]; y=fens.xyz[i, 2];
+        fens.xyz[i, :] .= (R*sin(a), y, R*(cos(a)-1))
+    end
+
+    # conn = fill(0, count(fes), 3)
+    # for i in 1:count(fes)
+    #     conn[i, :] .= fes.conn[i][2], fes.conn[i][3], fes.conn[i][1]
+    # end
+    # fromarray!(fes, conn)
+
+    mater = MatDeforElastIso(DeforModelRed3D, E, nu)
+    ocsys = CSys(3, 3, cylindrical!)
+    
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = formul.make(IntegDomain(fes, TriRule(1), thickness),  mater)
+    femm.drilling_stiffness_scale = 1.0e-4
+    femm.mult_el_size = 0.2
+    stiffness = formul.stiffness
+    associategeometry! = formul.associategeometry!
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # rigid diaphragm
+    l1 = selectnode(fens; box = Float64[-Inf Inf 0 0 -Inf Inf], inflate = tolerance)
+    for i in [1,3,5]
+        setebc!(dchi, l1, true, i)
+    end
+    # plane of symmetry perpendicular to Y
+    l1 = selectnode(fens; box = Float64[-Inf Inf L/2 L/2 -Inf Inf], inflate = tolerance)
+    for i in [2,4,6]
+        setebc!(dchi, l1, true, i)
+    end
+    # plane of symmetry perpendicular to X
+    l1 = selectnode(fens; box = Float64[0 0 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in [1,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the system matrix
+    associategeometry!(femm, geom0)
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    # Midpoint of the free edge
+    nl = selectnode(fens; box = Float64[sin(40/360*2*pi)*25 sin(40/360*2*pi)*25 L/2 L/2 -Inf Inf], inflate = tolerance)
+    lfemm = FEMMBase(IntegDomain(fes, TriRule(3)))
+    fi = ForceIntensity(FFlt[0, 0, -90, 0, 0, 0]);
+    F = distribloads(lfemm, geom0, dchi, fi, 3);
+    
+    # Solve
+    U = K\F
+    scattersysvec!(dchi, U[:])
+    result =   dchi.values[nl, 3][1]
+    @info "Solution: $(result), $(round(result/analyt_sol*100, digits = 4))%"
+
+    # Visualization
+    if visualize
+
+        # Generate a graphical display of resultants
+        # scalars = []
+        # for nc in 1:3
+        #     fld = fieldfromintegpoints(femm, geom0, dchi, :moment, nc, outputcsys = ocsys)
+        #     push!(scalars, ("m$nc", fld.values))
+        #     fld = elemfieldfromintegpoints(femm, geom0, dchi, :moment, nc, outputcsys = ocsys)
+        #     push!(scalars, ("em$nc", fld.values))
+        # end
+        # vtkwrite("scordelis_lo_examples-$(n)-m.vtu", fens, fes; scalars = scalars, vectors = [("u", dchi.values[:, 1:3])])
+        # scalars = []
+        # for nc in 1:3
+        #     fld = fieldfromintegpoints(femm, geom0, dchi, :membrane, nc, outputcsys = ocsys)
+        #     push!(scalars, ("n$nc", fld.values))
+        #     fld = elemfieldfromintegpoints(femm, geom0, dchi, :membrane, nc, outputcsys = ocsys)
+        #     push!(scalars, ("en$nc", fld.values))
+        # end
+        # vtkwrite("scordelis_lo_examples-$(n)-n.vtu", fens, fes; scalars = scalars, vectors = [("u", dchi.values[:, 1:3])])
+        scalars = []
+        for nc in 1:2
+            fld = fieldfromintegpoints(femm, geom0, dchi, :shear, nc, outputcsys = ocsys)
+            push!(scalars, ("q$nc", fld.values))
+            fld = elemfieldfromintegpoints(femm, geom0, dchi, :shear, nc, outputcsys = ocsys)
+            push!(scalars, ("eq$nc", fld.values))
+        end
+        vtkwrite("scordelis_lo_examples-$(n)-q.vtu", fens, fes; scalars = scalars, vectors = [("u", dchi.values[:, 1:3])])
+
+        # vtkwrite("scordelis_lo_examples-$(n)-uur.vtu", fens, fes; scalars = scalars, vectors = [("u", dchi.values[:, 1:3]), ("ur", dchi.values[:, 4:6])])
+
+        scattersysvec!(dchi, (L/8)/maximum(abs.(U)).*U)
+        update_rotation_field!(Rfield0, dchi)
+        plots = cat(plot_space_box([[0 0 -L/2]; [L/2 L/2 L/2]]),
+            #plot_nodes(fens),
+            plot_midsurface(fens, fes; x = geom0.values, facecolor = "rgb(12, 12, 123)"),
+            plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
+            dims = 1)
+        pl = render(plots)
+    end
+
+    result
+end
+
+function test_convergence(ns = [4, 6, 8, 16, 32], visualize = false)
+    formul = FEMMShellT3FFModule
+    @info "Scordelis-Lo shell"
+    results = []
+    for n in ns
+        v = _execute_dsg_model(formul, n, visualize)
+        push!(results, v/(-0.3024)*100)
+    end
+    return ns, results
+end
+
+end # module
+
+using .scordelis_lo_examples
+
+# Visualized internal resultants
+ns, results = scordelis_lo_examples.test_convergence([12*16,], true)
