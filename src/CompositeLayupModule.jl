6b5f5aa536e65ee6fbec8cdff14ac9f94cee8b02
Petr Krysl <Petr.Krysl.San.Diego@Gmail.com>
2021-12-27 09:52:51 -0800

    tangent moduli

--- src/CompositeLayupModule.jl
+++ src/CompositeLayupModule.jl
@@ -2,6 +2,7 @@ module CompositeLayupModule
 
 using FinEtools
 using LinearAlgebra: norm, Transpose, mul!
+using FinEtoolsDeforLinear.MatDeforLinearElasticModule: tangentmoduli!
 
 struct CompositeLayupPly{M}
     name::String
@@ -13,23 +14,31 @@ struct CompositeLayupPly{M}
 end
 
 function CompositeLayupPly(name, material::M, thickness, angle) where {M}
+    # First we extract the full three dimensional stiffness matrix of the
+    # material
     D = fill(0.0, 6, 6)
     # What if the material changes composition from point to point? What do we
     # do now?
     t::FFlt, dt::FFlt, loc::FFltMat, label::FInt = 0.0, 0.0, [0.0 0.0 0.0], 0
     tangentmoduli!(material,  D,  t, dt, loc, label)
+    # Next we reduce the 3d stiffness to plane stress
     Dps = fill(0.0, 3, 3)
     Dps[1:2, 1:2] = D[1:2, 1:2] -  (reshape(D[1:2,3], 2, 1) * reshape(D[3,1:2], 1, 2))/D[3, 3]
-    ix=[1, 2, 4];
+    ix = [1, 2, 4];
     for i = 1:3
         Dps[3,i] = Dps[i,3] = D[4, ix[i]];
     end
-    Dt = fill(0.0, 2, 2)
-    ix=[5, 6];
+    # And we also extract the transverse shear matrix
+    Dts = fill(0.0, 2, 2)
+    ix = [5, 6];
     for i = 1:2
-        Dt[i,i] = D[ix[i], ix[i]];
+        Dts[i,i] = D[ix[i], ix[i]];
     end
-    return CompositeLayupPly(name, material, thickness, angle, _A, _B, _C, _H)
+    # Both stiffness matrices are expressed on the reference coordinate system
+    # for the ply: direction 1 = fiber direction, direction 3 = normal to the
+    # ply
+
+    return CompositeLayupPly(name, material, thickness, angle, Dps, Dts)
 end
 
     
