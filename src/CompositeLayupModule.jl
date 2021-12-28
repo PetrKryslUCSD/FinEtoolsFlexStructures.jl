bc9773eded669ac2ad923b9749ef480d9e9fce19
Petr Krysl <Petr.Krysl.San.Diego@Gmail.com>
2021-12-27 18:33:11 -0800

    transformations

--- src/CompositeLayupModule.jl
+++ src/CompositeLayupModule.jl
@@ -1,9 +1,13 @@
 module CompositeLayupModule
 
 using FinEtools
-using LinearAlgebra: norm, Transpose, mul!
+using LinearAlgebra: norm, Transpose, mul!, I
+using FinEtoolsDeforLinear.MatDeforLinearElasticModule: tangentmoduli!
+using FinEtoolsDeforLinear.DeforModelRedModule: DeforModelRed2DStress
 
-struct CompositeLayupPly{M}
+abstract type AbstractPly end
+
+struct CompositeLayupPly{M} <: AbstractPly
     name::String
     material::M
     thickness::FFlt
@@ -13,44 +17,100 @@ struct CompositeLayupPly{M}
 end
 
 function CompositeLayupPly(name, material::M, thickness, angle) where {M}
+    # First we extract the full three dimensional stiffness matrix of the
+    # material
     D = fill(0.0, 6, 6)
     # What if the material changes composition from point to point? What do we
     # do now?
     t::FFlt, dt::FFlt, loc::FFltMat, label::FInt = 0.0, 0.0, [0.0 0.0 0.0], 0
     tangentmoduli!(material,  D,  t, dt, loc, label)
+    # Next we reduce the 3d stiffness to plane stress
+    # Both stiffness matrices are expressed on the reference coordinate system
+    # for the ply: direction 1 = fiber direction, direction 3 = normal to the
+    # ply
     Dps = fill(0.0, 3, 3)
     Dps[1:2, 1:2] = D[1:2, 1:2] -  (reshape(D[1:2,3], 2, 1) * reshape(D[3,1:2], 1, 2))/D[3, 3]
-    ix=[1, 2, 4];
+    ix = [1, 2, 4];
     for i = 1:3
         Dps[3,i] = Dps[i,3] = D[4, ix[i]];
     end
-    Dt = fill(0.0, 2, 2)
-    ix=[5, 6];
+    # And we also extract the transverse shear matrix
+    Dts = fill(0.0, 2, 2)
+    ix = [5, 6];
     for i = 1:2
-        Dt[i,i] = D[ix[i], ix[i]];
+        Dts[i,i] = D[ix[i], ix[i]];
     end
-    return CompositeLayupPly(name, material, thickness, angle, _A, _B, _C, _H)
+    
+    return CompositeLayupPly(name, material, thickness, angle, Dps, Dts)
 end
 
-    
-function _shell_material_stiffness(material)
-    # Compute the two material stiffness matrices: the plane stress matrix, and
-    # the transverse shear matrix
-    D = fill(0.0, 6, 6)
-    t::FFlt, dt::FFlt, loc::FFltMat, label::FInt = 0.0, 0.0, [0.0 0.0 0.0], 0
-    tangentmoduli!(material,  D,  t, dt, loc, label)
-    Dps = fill(0.0, 3, 3)
-    Dps[1:2, 1:2] = D[1:2, 1:2] -  (reshape(D[1:2,3], 2, 1) * reshape(D[3,1:2], 1, 2))/D[3, 3]
-    ix=[1, 2, 4];
-    for i = 1:3
-        Dps[3,i] = Dps[i,3] = D[4, ix[i]];
-    end
-    Dt = fill(0.0, 2, 2)
-    ix=[5, 6];
-    for i = 1:2
-        Dt[i,i] = D[ix[i], ix[i]];
-    end
-    return Dps, Dt
+struct CompositeLayup
+    name::String
+    plies::Vector{AbstractPly}
+end
+
+function plane_stress_stiffness!(cl::CompositeLayup, D)
+    # Transform into the composite layup coordinate system.
+end
+
+"""
+    plane_stress_T_matrix!(::Type{DeforModelRed2DStress},  Tinvm::Array{T, 2},
+     angle) where {T}
+
+Compute the transformation matrix between strain tensor components on the layup
+coordinate system into the ply coordinate system.
+
+`angle` = angle between the first basis vector of the layup coordinate system
+    and the first basis vector of the ply coordinate system
+"""
+function  plane_stress_T_matrix!(::Type{DeforModelRed2DStress}, Tm::Array{T, 2}, angle) where {T}
+    m=cos(angle); 
+    n=sin(angle); 
+    Tm[1, 1] =  (m^2);  Tm[1, 2] = (n^2);   Tm[1, 3] = (2*m*n)
+    Tm[2, 1] = (n^2);   Tm[2, 2] = (m^2);   Tm[2, 3] = (-2*m*n)
+    Tm[3, 1] = (-m*n);  Tm[3, 2] = (m*n);   Tm[3, 3] = (m*m-n*n)
+    return Tm
+end
+
+"""
+    plane_stress_Tinv_matrix!(::Type{DeforModelRed2DStress}, Tinvm::Array{T, 2}, angle) where {T}
+
+Compute the transformation matrix between strain tensor components on the ply
+coordinate system into the layout coordinate system.
+
+`angle` = angle between the first basis vector of the layup coordinate system
+    and the first basis vector of the ply coordinate system
+"""
+function  plane_stress_Tinv_matrix!(::Type{DeforModelRed2DStress}, Tinvm::Array{T, 2}, angle) where {T}
+    m=cos(angle); 
+    n=sin(angle); 
+    Tinvm[1, 1] = (m^2); Tinvm[1, 2] = (n^2);  Tinvm[1, 3] = (-2*m*n)
+    Tinvm[2, 1] = (n^2); Tinvm[2, 2] = (m^2);  Tinvm[2, 3] = (2*m*n)
+    Tinvm[3, 1] = (m*n); Tinvm[3, 2] = (-m*n); Tinvm[3, 3] = (m*m-n*n)
+    return Tinvm
+end
+
+"""
+    plane_stress_T_matrix_eng!(::Type{DeforModelRed2DStress},  Tinvm::Array{T, 2},
+     angle) where {T}
+
+Compute the transformation matrix between strain engineering components on the
+layup coordinate system into the ply coordinate system.
+
+`angle` = angle between the first basis vector of the layup coordinate system
+    and the first basis vector of the ply coordinate system
+
+The matrix of transformation is `Tme = R * Tm / R`, where `Tm`
+is the transformation matrix in tensor components, and `R` is the Reuter matrix,
+`R = [1 0 0; 0 1 0; 0 0 2]`.
+"""
+function  plane_stress_T_matrix_eng!(::Type{DeforModelRed2DStress}, Tm::Array{T, 2}, angle) where {T}
+    m=cos(angle); 
+    n=sin(angle); 
+    Tm[1, 1] =  (m^2);  Tm[1, 2] = (n^2);   Tm[1, 3] = (m*n)
+    Tm[2, 1] = (n^2);   Tm[2, 2] = (m^2);   Tm[2, 3] = (-m*n)
+    Tm[3, 1] = (-2*m*n);  Tm[3, 2] = (2*m*n);   Tm[3, 3] = (m*m-n*n)
+    return Tm
 end
 
 end # module
--- test/test_composite_layup.jl
+++ test/test_composite_layup.jl
@@ -12,10 +12,86 @@ function test()
     mater = MatDeforElastIso(DeforModelRed3D, rho, E, nu, 0.0)
     cm = CompositeLayupModule
     ply1 = cm.CompositeLayupPly("ply1", mater, thickness, angle)
-    @show ply1
+    @test ply1.angle == 45
     true
 end
 end
 using .mlayup1
 mlayup1.test()
+
+module mlayup2
+using FinEtools
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.CompositeLayupModule
+using Test
+function test()
+    E = 200e3*phun("MPa")
+    nu = 0.3;
+    rho= 8000*phun("KG/M^3");
+    thickness = 0.005*phun("m");
+    angle = 45.0
+    mater = MatDeforElastIso(DeforModelRed3D, rho, E, nu, 0.0)
+    CM = CompositeLayupModule
+    ply1 = CM.CompositeLayupPly("ply1", mater, thickness, 45.0)
+    ply2 = CM.CompositeLayupPly("ply2", mater, thickness, -45.0)
+    cl = CM.CompositeLayup("sample", [ply1, ply2])
+    @test cl.plies[2].angle == -45
+    true
+end
+end
+using .mlayup2
+mlayup2.test()
+
+
+module mlayup3
+using LinearAlgebra: norm, Transpose, mul!, I
+using FinEtools
+using FinEtoolsDeforLinear.DeforModelRedModule
+using FinEtoolsFlexStructures.CompositeLayupModule
+using Test
+function test()
+    CM = CompositeLayupModule
+    mtol = 1.0e-15
+    for dangle in [5 34 68 73 535 0 90 45 -135. -87]
+        angle = dangle/180*pi
+        Tm = fill(0.0, 3, 3)
+        CM.plane_stress_T_matrix!(DeforModelRedModule.DeforModelRed2DStress, Tm, angle)
+        Tinvm = fill(0.0, 3, 3)
+        CM.plane_stress_Tinv_matrix!(DeforModelRedModule.DeforModelRed2DStress, Tinvm, angle)
+        @test norm(Tm*Tinvm - I) < mtol
+    end
+    true
+end
+end
+using .mlayup3
+mlayup3.test()
+
+
+module mlayup4
+using LinearAlgebra: norm, Transpose, mul!, I
+using FinEtools
+using FinEtoolsDeforLinear.DeforModelRedModule
+using FinEtoolsFlexStructures.CompositeLayupModule
+using Test
+function test()
+    CM = CompositeLayupModule
+    mtol = 1.0e-15
+    R = [1 0 0; 0 1 0; 0 0 2]
+    Tm = fill(0.0, 3, 3)
+    Tme = fill(0.0, 3, 3)
+    for dangle in [5 34 68 73 535 0 90 45 -135. -87]
+        angle = dangle/180*pi
+        CM.plane_stress_T_matrix!(DeforModelRedModule.DeforModelRed2DStress, Tm, angle)
+        CM.plane_stress_T_matrix_eng!(DeforModelRedModule.DeforModelRed2DStress, Tme, angle)
+        # @show R * Tm / R, Tme
+        @test norm(R * Tm / R - Tme) < mtol
+        # Tinvm = fill(0.0, 3, 3)
+        # CM.plane_stress_Tinv_matrix!(DeforModelRedModule.DeforModelRed2DStress, Tinvm, angle)
+        # @test norm(Tm*Tinvm - I) < mtol
+    end
+    true
+end
+end
+using .mlayup4
+mlayup4.test()
 
