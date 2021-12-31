5ef688e4e994dd46091d2af49bb40112166ad615
Petr Krysl <pkrysl@ucsd.edu>
2021-12-31 08:14:06 -0800

    with example 3.3

--- test/test_composite_shell.jl
+++ test/test_composite_shell.jl
@@ -92,7 +92,7 @@ function test()
         # @show  maximum(dchi.values[:, i]) ./phun("mm")
         # @show minimum(dchi.values[:, i]) ./phun("mm")
     end
-    vtkwrite("plate-$npairs-uur.vtu", fens, fes; vectors = [("u", dchi.values[:, 1:3])])
+    # vtkwrite("plate-$npairs-uur.vtu", fens, fes; vectors = [("u", dchi.values[:, 1:3])])
     @test maximum(dchi.values[:, 3]) ./phun("mm") ≈ 0.21397084474482295
     true
 end
@@ -138,9 +138,7 @@ function test()
     end
     mcsys = CM.cartesian_csys((1, 2, 3))
     layup = CM.CompositeLayup("example_3.1", plies, mcsys)
-    @show length(layup.plies)
-    @show CM.thickness(layup)
-
+    
     fens, fes = T3block(ax,ay,nx,ny);
     fens.xyz = xyz3(fens)
     sfes = FESetShellT3()
@@ -196,7 +194,7 @@ function test()
         # @show  maximum(dchi.values[:, i]) ./phun("mm")
         # @show minimum(dchi.values[:, i]) ./phun("mm")
     end
-    vtkwrite("plate-$npairs-uur.vtu", fens, fes; vectors = [("u", dchi.values[:, 1:3])])
+    # vtkwrite("plate-$npairs-uur.vtu", fens, fes; vectors = [("u", dchi.values[:, 1:3])])
     @test maximum(dchi.values[:, 3]) ./phun("mm") ≈ 0.010121315960826563
     true
 end
@@ -222,35 +220,35 @@ function test()
     ax = ay = 2000*phun("mm")
     nx = ny = 8
     # ASFD/9310
-    E1 = 133860*phun("MPa")
-    E2 = 7706*phun("MPa")
-    G12 = 4306*phun("MPa")
+    E1 = 37880*phun("MPa")
+    E2 = 9407*phun("MPa")
+    G12 = 3405*phun("MPa")
     G13 = G12
-    nu12 = 0.301;
-    nu23 = 0.396
-    G23 = 2760*phun("MPa")
-    npairs = 10
-    thickness = 10*phun("mm");
+    nu12 = 0.299;
+    nu23 = 0.422
+    G23 = 3308*phun("MPa")
+    ply_thickness = 1*phun("mm");
     tolerance = ax/nx/100
     CM = CompositeLayupModule
 
     mater = CM.lamina_material(E1, E2, nu12, G12, G13, G23)
-    plies = CM.Ply[]
-    for p in 1:npairs
-        push!(plies, CM.Ply("ply_0_$p", mater, thickness/npairs/2, 0))
-        push!(plies, CM.Ply("ply_90_$p", mater, thickness/npairs/2, 90))
-    end
+    plies = CM.Ply[
+    CM.Ply("p", mater, ply_thickness, +45),
+    CM.Ply("p", mater, ply_thickness, -45),
+    CM.Ply("p", mater, ply_thickness, 0),
+    CM.Ply("p", mater, ply_thickness, 0),
+    CM.Ply("p", mater, ply_thickness, -45),
+    CM.Ply("p", mater, ply_thickness, +45),
+    ]
     mcsys = CM.cartesian_csys((1, 2, 3))
-    layup = CM.CompositeLayup("example_3.1", plies, mcsys)
-    @show length(layup.plies)
-    @show CM.thickness(layup)
+    layup = CM.CompositeLayup("example_3.3", plies, mcsys)
 
     fens, fes = T3block(ax,ay,nx,ny);
     fens.xyz = xyz3(fens)
     sfes = FESetShellT3()
     accepttodelegate(fes, sfes)
 
-    femm = formul.make(IntegDomain(fes, TriRule(1), thickness), layup)
+    femm = formul.make(IntegDomain(fes, TriRule(1), length(plies)*ply_thickness), layup)
 
     # Construct the requisite fields, geometry and displacement
     # Initialize configuration variables
@@ -261,12 +259,12 @@ function test()
 
     # Apply EBC's
     # Pin one of the corners
-    l1 = selectnode(fens; box = Float64[ax ax 0 0 -Inf Inf], inflate = tolerance)
-    for i in [1,2, 3,]
+    l1 = selectnode(fens; box = Float64[0 0 0 0 -Inf Inf], inflate = tolerance)
+    for i in [1, 2, 3,]
         setebc!(dchi, l1, true, i)
     end
     # Roller at the other
-    l1 = selectnode(fens; box = Float64[0 0 0 0 -Inf Inf], inflate = tolerance)
+    l1 = selectnode(fens; box = Float64[ax ax 0 0 -Inf Inf], inflate = tolerance)
     for i in [2,]
         setebc!(dchi, l1, true, i)
     end
@@ -284,24 +282,26 @@ function test()
 
     # Edge load
     bfes = meshboundary(fes)
-    l1 = selectelem(fens, bfes, box = Float64[-Inf Inf 0 0 0 0 ], inflate = tolerance)
+    l1 = selectelem(fens, bfes, box = Float64[0 0 -Inf Inf 0 0 ], inflate = tolerance)
     lfemm = FEMMBase(IntegDomain(subset(bfes, l1), GaussRule(1, 2)))
-    fi = ForceIntensity(FFlt[0, 0.1*phun("MPa")*thickness, 0, 0, 0, 0]);
+    fi = ForceIntensity(FFlt[-200*phun("N/mm"), 0, 0, 0, 0, 0]);
     F = distribloads(lfemm, geom0, dchi, fi, 1);
-    l1 = selectelem(fens, bfes, box = Float64[-Inf Inf ay ay 0 0 ], inflate = tolerance)
+    l1 = selectelem(fens, bfes, box = Float64[ax ax -Inf Inf 0 0 ], inflate = tolerance)
     lfemm = FEMMBase(IntegDomain(subset(bfes, l1), GaussRule(1, 2)))
-    fi = ForceIntensity(FFlt[0, -0.1*phun("MPa")*thickness, 0, 0, 0, 0]);
+    fi = ForceIntensity(FFlt[200*phun("N/mm"), 0, 0, 0, 0, 0]);
     F += distribloads(lfemm, geom0, dchi, fi, 1);
     
     # Solve
     U = K\F
     scattersysvec!(dchi, U[:])
     for i in 1:3
-        # @show  maximum(dchi.values[:, i]) ./phun("mm")
+        # @show maximum(dchi.values[:, i]) ./phun("mm")
         # @show minimum(dchi.values[:, i]) ./phun("mm")
     end
-    vtkwrite("plate-$npairs-uur.vtu", fens, fes; vectors = [("u", dchi.values[:, 1:3])])
-    @test maximum(dchi.values[:, 3]) ./phun("mm") ≈ 0.010121315960826563
+    # vtkwrite("plate-3.3-uur.vtu", fens, fes; vectors = [("u", dchi.values[:, 1:3])])
+     # The target value is 1.667 mm at either of the loaded edges;
+    # so the total displacement is approximately 2*1.667=3.334 
+    @test maximum(dchi.values[:, 1]) ./phun("mm") ≈ 3.3161098653904544
     true
 end
 end
