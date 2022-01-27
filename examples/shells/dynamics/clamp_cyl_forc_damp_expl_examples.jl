6423043a8c830b50018b8f0057ce2adcb05b8dbe
Petr Krysl <pkrysl@ucsd.edu>
2022-01-27 14:22:51 -0800

    serial plus parallel

--- examples/shells/dynamics/clamp_cyl_forc_damp_expl_examples.jl
+++ examples/shells/dynamics/clamp_cyl_forc_damp_expl_examples.jl
@@ -110,7 +110,7 @@ function parmul!(R, tbs)
     R
 end
 
-function parloop!(M, K, ksi, U0, V0, tend, dt, force!, peek)
+function parloop!(M, K, ksi, U0, V0, tend, dt, force!, peek, nthr)
     U1 = deepcopy(U0)
     V1 = deepcopy(U0)
     A0 = deepcopy(U0)
@@ -128,7 +128,8 @@ function parloop!(M, K, ksi, U0, V0, tend, dt, force!, peek)
     end
     t = 0.0
     
-    @show nth = Base.Threads.nthreads()
+    nth = (nthr == 0 ? Base.Threads.nthreads() : nthr)
+    @info "$nth threads used"
     chunk = Int(floor(length(U1) / nth))
     threadbuffs = ThreadBuffer[];
     for th in 1:nth
@@ -176,10 +177,9 @@ cylindrical!(csmatout::FFltMat, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
     return csmatout
 end
 
-function _execute(n = 8, thickness = 0.01, visualize = true)
+const visualize = true
 
-    # @info "Mesh: $n elements per side"
-    # Mesh
+function _execute_parallel(n = 64, thickness = 0.01, nthr = 0)
     tolerance = min(R, L) / n  / 100
     
     tolerance = R/n/1000
@@ -205,14 +205,14 @@ function _execute(n = 8, thickness = 0.01, visualize = true)
     femm.drilling_mass_scale = 1.0
     femm.drilling_stiffness_scale = 1.0
 
-# Construct the requisite fields, geometry and displacement
-# Initialize configuration variables
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
     geom0 = NodalField(fens.xyz)
     u0 = NodalField(zeros(size(fens.xyz,1), 3))
     Rfield0 = initial_Rfield(fens)
     dchi = NodalField(zeros(size(fens.xyz,1), 6))
 
-# Apply EBC's
+    # Apply EBC's
     l1 = connectednodes(meshboundary(fes))
     for i in 1:6
         setebc!(dchi, l1, true, i)
@@ -220,12 +220,12 @@ function _execute(n = 8, thickness = 0.01, visualize = true)
     applyebc!(dchi)
     numberdofs!(dchi);
 
-# Assemble the system matrix
+    # Assemble the system matrix
     FEMMShellT3FFModule.associategeometry!(femm, geom0)
     K = FEMMShellT3FFModule.stiffness(femm, geom0, u0, Rfield0, dchi);
     M = FEMMShellT3FFModule.mass(femm, SysmatAssemblerSparseDiag(), geom0, dchi);
     
-# Solve
+    # Solve
     function pwr(K, M)
         invM = fill(0.0, size(M, 1))
         invM .= 1.0 ./ (vec(diag(M)))
@@ -312,7 +312,7 @@ function _execute(n = 8, thickness = 0.01, visualize = true)
     end
 
     @info "$nsteps steps"
-    parloop!(M, K, ksi, U0, V0, nsteps*dt, dt, force!, peek)
+    parloop!(M, K, ksi, U0, V0, nsteps*dt, dt, force!, peek, nthr)
     
     # @gp  "set terminal windows 0 "  :-
 
@@ -337,18 +337,187 @@ function _execute(n = 8, thickness = 0.01, visualize = true)
     vtkwritecollection("clamp_cyl_forc_damp_expl_$n", fens, fes, times; vectors = vectors)
 end
 
-function test_convergence(ns = [4*64])
-    @info "Clamped cylinder vibration"
+function _execute_serial(n = 64, thickness = 0.01)
+    tolerance = min(R, L) / n  / 100
+    
+    tolerance = R/n/1000
+    fens, fes = T3block(360.0, L, n, 4*n);
+    fens.xyz = xyz3(fens)
+    for i in 1:count(fens)
+        a=fens.xyz[i, 1]; y=fens.xyz[i, 2];
+        fens.xyz[i, :] .= (R*sin(a/180*pi), y-L/2, R*cos(a/180*pi))
+    end
+    bfes = meshboundary(fes)
+    candidates = connectednodes(bfes)
+    fens, fes = mergenodes(fens, fes, tolerance, candidates)
+    bfes = meshboundary(fes)
+    @info "Mesh $(count(fens)) nodes, $(count(fes)) elements"
+    
+    mater = MatDeforElastIso(DeforModelRed3D, rho, E, nu, 0.0)
+    ocsys = CSys(3, 3, cylindrical!)
+
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = FEMMShellT3FFModule.make(IntegDomain(fes, TriRule(1), thickness), ocsys, mater)
+    # Set up
+    femm.drilling_mass_scale = 1.0
+    femm.drilling_stiffness_scale = 1.0
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    l1 = connectednodes(meshboundary(fes))
+    for i in 1:6
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the system matrix
+    FEMMShellT3FFModule.associategeometry!(femm, geom0)
+    K = FEMMShellT3FFModule.stiffness(femm, geom0, u0, Rfield0, dchi);
+    M = FEMMShellT3FFModule.mass(femm, SysmatAssemblerSparseDiag(), geom0, dchi);
+    
+    # Solve
+    function pwr(K, M)
+        invM = fill(0.0, size(M, 1))
+        invM .= 1.0 ./ (vec(diag(M)))
+        v = rand(size(M, 1))
+        w = fill(0.0, size(M, 1))
+        for i in 1:30
+            mul!(w, K, v)
+            wn = norm(w)
+            w .*= (1.0/wn)
+            v .= invM .* w
+            vn = norm(v)
+            v .*= (1.0/vn)
+        end
+        sqrt((v' * K * v) / (v' * M * v))
+    end
+    @time omega_max = pwr(K, M)
+    @show omega_max
+
+    # @time evals, evecs, nconv = eigs(Symmetric(K), Symmetric(M); nev=1, which=:LM, explicittransform=:none)
+    # @show sqrt(evals)
+    # @show omega_max = sqrt(evals[1])
+
+    @show dt = Float64(0.9* 2/omega_max) * (sqrt(1+ksi^2) - ksi)
+
+    U0 = gathersysvec(dchi)
+    V0 = deepcopy(U0)
+    
+    qpoint = selectnode(fens; nearestto=[0 -L/4 R])[1]
+    cpoint = selectnode(fens; nearestto=[0 0 R])[1]
+    applyebc!(dchi)
+    numberdofs!(dchi);
+    qpointdof = dchi.dofnums[qpoint, 3]
+    cpointdof = dchi.dofnums[cpoint, 3]
+    cpointdof6 = dchi.dofnums[cpoint, 6]
+    
+    function computetrac!(forceout::FFltVec, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+        dx = XYZ[1] - fens.xyz[qpoint, 1]
+        dy = XYZ[2] - fens.xyz[qpoint, 2]
+        dz = XYZ[3] - fens.xyz[qpoint, 3]
+        mag = 10^5*exp(-20/R*sqrt(dx^2+dy^2+dz^2))
+        r = vec(XYZ); r[2] = 0.0
+        r .= vec(r)/norm(vec(r))
+        theta = atan(r[3], r[1])
+        forceout[1:3] .= r*mag
+        forceout[4:6] .= 0.0
+        return forceout
+    end
+
+    # Sinusoidal loading on the surface of the shell
+    # lfemm = FEMMBase(IntegDomain(fes, TriRule(3)))
+    # fi = ForceIntensity(FFlt, 6, computetrac!);
+    # Fmag = distribloads(lfemm, geom0, dchi, fi, 2);
+
+    # function force!(F, t)
+    #     if t < 2 * (2*pi/omegaf)
+    #         F .= sin(omegaf*t) .* Fmag
+    #     else
+    #         F .= 0.0
+    #     end
+    #     # @show norm(F)
+    #     return F
+    # end
+
+    # Suddenly applied constant force at a node
+    Fmag = fill(0.0, dchi.nfreedofs)
+    Fmag[qpointdof] = -1.0
+    function force!(F, t)
+        F .= Fmag
+        return F
+    end
+    
+    nsteps = Int(round(tend/dt))
+    cdeflections = fill(0.0, nsteps+1)
+    displacements = []
+    nbtw = Int(round(nsteps/100))
+
+
+    peek(step, U, t) = begin
+        cdeflections[step+1] = U[cpointdof]
+        if rem(step+1, nbtw) == 0
+            push!(displacements, deepcopy(U))
+        end
+        nothing
+    end
+
+    @info "$nsteps steps"
+    loop!(M, K, ksi, U0, V0, nsteps*dt, dt, force!, peek)
+    
+    # @gp  "set terminal windows 0 "  :-
+
+    # color = "blue"  
+    @gp "clear"
+    @gp  :- collect(0.0:dt:(nsteps*dt)) cdeflections " lw 2 lc rgb '$color' with lines title 'Deflection at the center' "  :-
+
+    @gp  :- "set xlabel 'Time'" :-
+    @gp  :- "set ylabel 'Deflection'" :-
+    @gp  :- "set title 'Free-floating plate'"
+
+
+    # Visualization
+    @info "Dumping visualization"
+    times = Float64[]
+    vectors = []
+    for i in 1:length(displacements)
+        scattersysvec!(dchi, displacements[i])
+        push!(vectors, ("U", deepcopy(dchi.values[:, 1:3])))
+        push!(times, i*dt*nbtw)
+    end
+    vtkwritecollection("clamp_cyl_forc_damp_expl_$n", fens, fes, times; vectors = vectors)
+end
+
+function test_serial(ns = [4*64])
+    @info "Clamped cylinder: serial"
     for n in ns
-        _execute(n, 0.01, !false)
+        _execute_serial(n, 0.01)
+    end
+    return true
+end
+
+function test_parallel(ns = [4*64], nthr = 0)
+    @info "Clamped cylinder: parallel"
+    for n in ns
+        _execute_parallel(n, 0.01, nthr)
     end
     return true
 end
 
 function allrun()
     println("#####################################################")
-    println("# test_convergence ")
-    test_convergence()
+    println("# test_serial ")
+    test_serial()
+    println("#####################################################")
+    println("# test_parallel ")
+    test_parallel()
     return true
 end # function allrun
 
