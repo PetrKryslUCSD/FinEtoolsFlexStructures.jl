707d4aee595cdf79897cb3c8d3ac2aec16b40993
Petr Krysl <pkrysl@ucsd.edu>
2022-02-01 19:03:15 -0800

    spherical cap example

---
+++ examples/shells/dynamics/spherical_cap_expl_examples.jl
@@ -0,0 +1,262 @@
+"""
+Spherical cap, transient vibration.
+
+The cylinder is banged at the initial time (i.e. it is given an initial velocity).
+"""
+module spherical_cap_expl_examples
+
+using LinearAlgebra
+using SparseArrays
+using Arpack
+using FinEtools
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures
+using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
+using FinEtoolsFlexStructures.AssemblyModule
+using FinEtoolsFlexStructures.FEMMShellT3FFModule
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, update_rotation_field!
+using SymRCM
+using VisualStructures: default_layout_3d, plot_nodes, plot_midline, render, plot_space_box, plot_midsurface, space_aspectratio, save_to_json
+using PlotlyJS
+using Gnuplot; #@gp "clear"
+using FinEtools.MeshExportModule.VTKWrite: vtkwritecollection
+using ThreadedSparseCSR
+using UnicodePlots
+using InteractiveUtils
+using BenchmarkTools
+
+const E = 10.5e6*phun("psi")
+const nu = 0.3;
+const rho = 2.45e-4*phun("lbf*s^2/in^4")
+const R = 22.27*phun("in")
+const h = 0.41*phun("in")
+const Rxy = (R)*sin(26.67/180*pi)
+const q = 600*phun("psi")
+const ksi = 0.0
+const omegad = 2*pi*1000
+const color = "red"
+const tend = 6e-4
+const visualize = true
+
+function parloop_csr!(M, K, ksi, U0, V0, tend, dt, force!, peek, nthr)
+    U = deepcopy(U0)
+    V = deepcopy(U0)
+    A = deepcopy(U0)
+    F = deepcopy(U0)
+    E = deepcopy(U0)
+    C = deepcopy(U0)
+    C .= (ksi*2*omegad) .* vec(diag(M))
+    invMC = deepcopy(U0)
+    invMC .= 1.0 ./ (vec(diag(M)) .+ (dt/2) .* C)
+    nsteps = Int64(round(tend/dt))
+    if nsteps*dt < tend
+        dt = tend / (nsteps+1)
+    end
+
+    nth = (nthr == 0 ? Base.Threads.nthreads() : nthr)
+    @info "$nth threads used"
+    
+    t = 0.0
+    # Initial Conditions
+    @. U = U0; @. V = V0
+    A .= invMC .* force!(F, t);
+    peek(0, U, t)
+    @time for step in 1:nsteps
+        # Displacement update
+        @. U += dt*V + ((dt^2)/2)*A; 
+        # External loading
+        force!(F, t);
+        # Add elastic restoring forces
+        F .-= ThreadedSparseCSR.bmul!(E, K, U)
+        @inbounds @simd for i in eachindex(U)
+            _Fi = F[i]; _Ai = A[i]; _Vi = V[i]
+            # Add damping forces
+            _Fi -= C[i] * (_Vi + (dt/2) * _Ai)
+            # Compute the new acceleration.
+            _A1i = invMC[i] * _Fi
+            A[i] = _A1i
+            # Update the velocity
+            V[i] += (dt/2)* (_Ai + _A1i);
+        end
+        t = t + dt
+        peek(step, U, t)
+    end
+end
+
+function _execute_parallel_csr(n = 64, nthr = 0)
+    color = "red"
+    tolerance = min(R, h) / n  / 100
+    
+    tolerance = R/n/1000
+    fens, fes = Q4circlen(Rxy, n)
+    fens, fes = Q4toT3(fens, fes)
+    
+    fens.xyz = xyz3(fens)
+    for i in 1:count(fens)
+        x=fens.xyz[i, 1]; y=fens.xyz[i, 2];
+        fens.xyz[i, :] .= (x, y, sqrt(R^2-x^2-y^2))
+    end
+
+    # bfes = meshboundary(fes)
+    # candidates = connectednodes(bfes)
+    # fens, fes = mergenodes(fens, fes, tolerance, candidates)
+    bfes = meshboundary(fes)
+    @info "Mesh $(count(fens)) nodes, $(count(fes)) elements"
+
+    # Renumber the nodes
+    femm = FEMMBase(IntegDomain(fes, TriRule(1)))
+    C = connectionmatrix(femm, count(fens))
+    perm = symrcm(C)
+    
+    mater = MatDeforElastIso(DeforModelRed3D, rho, E, nu, 0.0)
+    
+    sfes = FESetShellT3()
+    accepttodelegate(fes, sfes)
+    femm = FEMMShellT3FFModule.make(IntegDomain(fes, TriRule(1), h), mater)
+    # Set up
+    femm.drilling_mass_scale = 1.0
+    femm.drilling_stiffness_scale = 1.0
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    lx = selectnode(fens; box = Float64[0 0 -Inf Rxy-10*tolerance -Inf Inf], inflate = tolerance)
+    ly = selectnode(fens; box = Float64[-Inf Rxy-10*tolerance 0 0 -Inf Inf], inflate = tolerance)
+    lo = setdiff(connectednodes(meshboundary(fes)), vcat(lx, ly))
+    for i in [2, 4, 6]
+        setebc!(dchi, ly, true, i)
+    end
+    for i in [1, 5, 6]
+        setebc!(dchi, lx, true, i)
+    end
+    for i in 1:6
+        setebc!(dchi, lo, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi, perm);
+    # numberdofs!(dchi);
+
+    # Assemble the system matrix
+    FEMMShellT3FFModule.associategeometry!(femm, geom0)
+    SM = FinEtoolsFlexStructures.AssemblyModule
+    K = FEMMShellT3FFModule.stiffness(femm, SM.SysmatAssemblerSparseCSRSymm(0.0), geom0, u0, Rfield0, dchi);
+    M = FEMMShellT3FFModule.mass(femm, SysmatAssemblerSparseDiag(), geom0, dchi);
+    
+    # Solve
+    function pwr(K, M)
+        invM = fill(0.0, size(M, 1))
+        invM .= 1.0 ./ (vec(diag(M)))
+        v = rand(size(M, 1))
+        w = fill(0.0, size(M, 1))
+        for i in 1:30
+            ThreadedSparseCSR.bmul!(w, K, v)
+            wn = norm(w)
+            w .*= (1.0/wn)
+            v .= invM .* w
+            vn = norm(v)
+            v .*= (1.0/vn)
+        end
+        sqrt((v' * (K * v)) / (v' * M * v))
+    end
+    @time omega_max = pwr(K, M)
+    @show omega_max
+
+    # @time evals, evecs, nconv = eigs(Symmetric(K), Symmetric(M); nev=1, which=:LM, explicittransform=:none)
+    # @show sqrt(evals)
+    # @show omega_max = sqrt(evals[1])
+
+    @show dt = Float64(0.9* 2/omega_max) * (sqrt(1+ksi^2) - ksi)
+
+    U0 = gathersysvec(dchi)
+    V0 = deepcopy(U0)
+    
+    
+    cpoint = selectnode(fens; nearestto=[0 0 R])[1]
+    
+    
+    cpointdof = dchi.dofnums[cpoint, 3]
+    
+    
+    function computetrac!(forceout::FFltVec, XYZ::FFltMat, tangents::FFltMat, fe_label::FInt)
+        r = vec(XYZ); 
+        r .= vec(r)/norm(vec(r))
+        forceout[1:3] .= -r*q
+        forceout[4:6] .= 0.0
+        return forceout
+    end
+
+    # Distributed loading on the surface of the shell
+    lfemm = FEMMBase(IntegDomain(fes, TriRule(3)))
+    fi = ForceIntensity(FFlt, 6, computetrac!);
+    Fmag = distribloads(lfemm, geom0, dchi, fi, 2);
+
+    function force!(F, t)
+        F .= Fmag
+        return F
+    end
+
+    nsteps = Int(round(tend/dt))
+    cdeflections = fill(0.0, nsteps+1)
+    displacements = []
+    nbtw = Int(round(nsteps/100))
+
+
+    peek(step, U, t) = begin
+        cdeflections[step+1] = U[cpointdof]
+        if rem(step+1, nbtw) == 0
+            push!(displacements, deepcopy(U))
+        end
+        nothing
+    end
+
+    @info "$nsteps steps"
+    parloop_csr!(M, K, ksi, U0, V0, nsteps*dt, dt, force!, peek, nthr)
+    
+    if visualize
+        # @gp  "set terminal windows 0 "  :-
+        # @gp "clear"
+        @gp  :- collect(0.0:dt:(nsteps*dt)) cdeflections/phun("in") " lw 2 lc rgb '$color' with lines title 'Deflection at the center' "  :-
+
+        @gp  :- "set xlabel 'Time'" :-
+        @gp  :- "set ylabel 'Deflection'" :-
+        @gp  :- "set title 'Free-floating plate'"
+
+
+        # Visualization
+        @info "Dumping visualization"
+        times = Float64[]
+        vectors = []
+        for i in 1:length(displacements)
+            scattersysvec!(dchi, displacements[i])
+            push!(vectors, ("U", deepcopy(dchi.values[:, 1:3])))
+            push!(times, i*dt*nbtw)
+        end
+        vtkwritecollection("spherical_cap_expl_$n", fens, fes, times; vectors = vectors)
+    end
+end
+
+function test_parallel_csr(ns = [4*64], nthr = 0)
+    @info "Clamped cylinder: parallel CSR"
+    for n in ns
+        _execute_parallel_csr(n, nthr)
+    end
+    return true
+end
+
+function allrun(ns = [80])
+    println("#####################################################")
+    println("# test_parallel_csr ")
+    test_parallel_csr(ns)
+    return true
+end # function allrun
+
+@info "All examples may be executed with "
+println("using .$(@__MODULE__); $(@__MODULE__).allrun()")
+
+end # module
+nothing
