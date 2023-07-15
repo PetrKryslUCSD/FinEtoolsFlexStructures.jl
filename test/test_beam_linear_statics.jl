81a5b9bc33c817213a9f1ca7a13d944ae8ce29c1
Petr Krysl <pkrysl@ucsd.edu>
2023-07-14 19:55:51 -0700

    add conv test for very thin beams

--- test/test_beam_linear_statics.jl
+++ test/test_beam_linear_statics.jl
@@ -1008,3 +1008,203 @@ nothing
 nothing
 
 
+module mccbeamThin1
+#  Clamped-clamped beam with distributed load. Soft axis bending
+
+using FinEtools
+using FinEtools.AlgoBaseModule: solve!, matrix_blocked
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.CrossSectionModule: CrossSectionRectangle
+using FinEtoolsFlexStructures.MeshFrameMemberModule: frame_member, merge_members
+using FinEtoolsFlexStructures.FEMMRITBeamModule
+using FinEtoolsFlexStructures.FEMMRITBeamModule: FEMMRITBeam
+stiffness = FEMMRITBeamModule.stiffness
+distribloads_global = FEMMRITBeamModule.distribloads_global
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, update_rotation_field!
+using LinearAlgebra: dot
+using Arpack
+using LinearAlgebra
+using SparseArrays
+using Test
+# using VisualStructures: plot_nodes, plot_midline, render, plot_space_box, plot_solid, space_aspectratio, save_to_json
+
+function test(b = 2)
+    E=30002*1000.0;#30002ksi
+    # b=2;#in
+    h=18;#in
+    L=240;# in
+    q=1/b;# 1200 lbf/ft
+
+    # Cross-sectional properties
+    A=b*h;#in^2
+    I2=b*h^3/12;#cm^4
+    I3=b^3*h/12;#cm^4
+    k_s = 5/6
+    nu=0.0;
+
+    ##
+    # Exact deformation under the load
+
+    deflex=norm(q)*L^4/(384*E*I3);
+
+    # Cross-sectional properties
+    cs = CrossSectionRectangle(s -> b, s -> h, s -> [-1.0, 0.0, 0.0], k_s)
+
+    # Select the number of elements per leg.
+    n = 16;
+    members = []
+    push!(members, frame_member([0 -L/2 0; 0 L/2 0], n, cs))
+    fens, fes = merge_members(members; tolerance = L / 10000);
+
+    # Material properties
+    material = MatDeforElastIso(DeforModelRed3D, E, nu)
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    tipl = selectnode(fens; box = Float64[0 0 0 0 0 0], inflate = L/10000)
+    l1 = selectnode(fens; box = Float64[0 0 -L/2 -L/2 0 0], inflate = L/10000)
+    for i in [1,2,3,4,5,6] # clamped
+        setebc!(dchi, l1, true, i)
+    end
+    l1 = selectnode(fens; box = Float64[0 0 +L/2 +L/2 0 0], inflate = L/10000)
+    for i in [1,2,3,4,5,6] # clamped
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the global discrete system
+    femm = FEMMRITBeam(IntegDomain(fes, GaussRule(1, 2)), material)
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    fi = ForceIntensity(FFlt[q, 0, 0]);
+    F = distribloads_global(femm, geom0, u0, Rfield0, dchi, fi);
+
+    # Solve the static problem
+    solve!(dchi, K, F)
+    return dchi.values[tipl, 1][1] / deflex * 100, b/L
+    # @test norm(dchi.values[tipl, 1] .- deflex) / deflex < 1.0e-5
+
+
+    # scaling = 1e4
+    # dchi.values .*= scaling
+    # update_rotation_field!(Rfield0, dchi)
+    # plots = cat(plot_space_box([[-L/2 -L/2 -L/2]; [L/2 L/2 L/2]]),
+    # plot_nodes(fens),
+    # plot_solid(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values); dims = 1)
+    # pl = render(plots)
+    # true
+end
+for b in [2  0.02 0.0002  ]
+    @test abs(test(b)[1] - 98.44) / 100 <= 1e-2
+end
+end # module
+
+
+module mccbeamThin2
+#  Clamped-clamped beam with distributed load. Soft axis bending
+
+using FinEtools
+using FinEtools.AlgoBaseModule: solve!, matrix_blocked
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.CrossSectionModule: CrossSectionRectangle
+using FinEtoolsFlexStructures.MeshFrameMemberModule: frame_member, merge_members
+using FinEtoolsFlexStructures.FEMMCorotBeamModule
+using FinEtoolsFlexStructures.FEMMCorotBeamModule: FEMMCorotBeam
+stiffness = FEMMCorotBeamModule.stiffness
+distribloads_global = FEMMCorotBeamModule.distribloads_global
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, update_rotation_field!
+using LinearAlgebra: dot
+using Arpack
+using LinearAlgebra
+using SparseArrays
+using Test
+# using VisualStructures: plot_nodes, plot_midline, render, plot_space_box, plot_solid, space_aspectratio, save_to_json
+
+function test(b = 2)
+    E=30002*1000.0;#30002ksi
+    # b=2;#in
+    h=18;#in
+    L=240;# in
+    q=1/b;# 1200 lbf/ft
+
+    # Cross-sectional properties
+    A=b*h;#in^2
+    I2=b*h^3/12;#cm^4
+    I3=b^3*h/12;#cm^4
+    k_s = 5/6
+    nu=0.0;
+
+    ##
+    # Exact deformation under the load
+
+    deflex=norm(q)*L^4/(384*E*I3);
+
+    # Cross-sectional properties
+    cs = CrossSectionRectangle(s -> b, s -> h, s -> [-1.0, 0.0, 0.0], k_s)
+
+    # Select the number of elements per leg.
+    n = 16;
+    members = []
+    push!(members, frame_member([0 -L/2 0; 0 L/2 0], n, cs))
+    fens, fes = merge_members(members; tolerance = L / 10000);
+
+    # Material properties
+    material = MatDeforElastIso(DeforModelRed3D, E, nu)
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    tipl = selectnode(fens; box = Float64[0 0 0 0 0 0], inflate = L/10000)
+    l1 = selectnode(fens; box = Float64[0 0 -L/2 -L/2 0 0], inflate = L/10000)
+    for i in [1,2,3,4,5,6] # clamped
+        setebc!(dchi, l1, true, i)
+    end
+    l1 = selectnode(fens; box = Float64[0 0 +L/2 +L/2 0 0], inflate = L/10000)
+    for i in [1,2,3,4,5,6] # clamped
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    # Assemble the global discrete system
+    femm = FEMMCorotBeam(IntegDomain(fes, GaussRule(1, 2)), material)
+    K = stiffness(femm, geom0, u0, Rfield0, dchi);
+
+    fi = ForceIntensity(FFlt[q, 0, 0]);
+    F = distribloads_global(femm, geom0, u0, Rfield0, dchi, fi);
+
+    # Solve the static problem
+    solve!(dchi, K, F)
+    return dchi.values[tipl, 1][1] / deflex * 100, b/L
+    # @test norm(dchi.values[tipl, 1] .- deflex) / deflex < 1.0e-5
+
+
+    # scaling = 1e4
+    # dchi.values .*= scaling
+    # update_rotation_field!(Rfield0, dchi)
+    # plots = cat(plot_space_box([[-L/2 -L/2 -L/2]; [L/2 L/2 L/2]]),
+    # plot_nodes(fens),
+    # plot_solid(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values); dims = 1)
+    # pl = render(plots)
+    # true
+end
+for b in [2  0.02 0.0002  ]
+    # @show test(b)
+    @test abs(test(b)[1] - 100) / 100 <= 1e-2
+end
+end # module
+
+nothing
+
