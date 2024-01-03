157d0f6d9f19132984d7a6cf18a5d338d6aa44d9
Petr Krysl <pkrysl@ucsd.edu>
2024-01-03 11:24:43 -0800

    add assembly for csr matrices

--- src/AssemblyModule.jl
+++ src/AssemblyModule.jl
@@ -1,11 +1,56 @@
 """
     AssemblyModule
-
 Module for assemblers  of system matrices and vectors.
 """
 module AssemblyModule
 
 __precompile__(true)
 
+using FinEtools
+import FinEtools.AssemblyModule: startassembly!, assemble!, makematrix!
+using SparseArrays
+using SparseMatricesCSR
+using  LinearAlgebra: diag
+
+"""
+    SysmatAssemblerSparseCSRSymm{A<:AbstractSysmatAssembler} <: AbstractSysmatAssembler
+
+Assembler for a **symmetric square** matrix  assembled from symmetric square
+matrices. The matrix is stored in the Compressed Sparse Row (CSR) format.
+"""
+mutable struct SysmatAssemblerSparseCSRSymm{A<:AbstractSysmatAssembler} <: AbstractSysmatAssembler
+  a::A
+end
+
+function SysmatAssemblerSparseCSRSymm(zero::T=0.0) where {T<:Number}
+    return SysmatAssemblerSparseCSRSymm(SysmatAssemblerSparse(zero))
+end
+
+function startassembly!(self::SysmatAssemblerSparseCSRSymm,
+    expected_ntriples::IT,
+    row_nalldofs::IT,
+    col_nalldofs::IT;
+    force_init = false) where {IT <: Integer}
+    self.a = startassembly!(self.a, expected_ntriples, row_nalldofs, col_nalldofs; force_init)
+    return self
+end
+
+function assemble!(self::SysmatAssemblerSparseCSRSymm,
+    mat::MBT,
+    dofnums_row::CIT,
+    dofnums_col::CIT) where {MBT, CIT}
+    assemble!(self.a, mat, dofnums_row, dofnums_col)
+    return self
+end
+
+function makematrix!(self::SysmatAssemblerSparseCSRSymm)
+    S = makematrix!(self.a)
+    I, J, V = findnz(S)
+    # Use the CSR package.
+    S = SparseMatricesCSR.sparsecsr(I, J, V, size(S)...)
+    return S
+end
+
 
 end
+
--- test/test_utilities.jl
+++ test/test_utilities.jl
@@ -1,1 +1,54 @@
 
+
+module mutil001
+
+using Test
+using LinearAlgebra
+using FinEtools
+using FinEtoolsDeforLinear
+using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
+using FinEtoolsFlexStructures.FEMMShellT3FFModule
+using FinEtoolsFlexStructures.AssemblyModule
+using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, update_rotation_field!
+
+function _test()
+   SM = AssemblyModule
+   a = SM.SysmatAssemblerSparseCSRSymm(0.0)
+   SM.startassembly!(a, 5 * 5 * 3, 7, 7)
+   m = [0.24406   0.599773    0.833404  0.0420141
+    0.786024  0.00206713  0.995379  0.780298
+    0.845816  0.198459    0.355149  0.224996]
+    # @show    m'*m
+   SM.assemble!(a, m'*m, [5 2 1 4], [5 2 1 4])
+   m = [0.146618  0.53471   0.614342    0.737833
+     0.479719  0.41354   0.00760941  0.836455
+     0.254868  0.476189  0.460794    0.00919633
+     0.159064  0.261821  0.317078    0.77646
+     0.643538  0.429817  0.59788     0.958909]
+     # @show    m'*m
+   SM.assemble!(a, m'*m, [2 3 1 5], [2 3 1 5])
+   A = SM.makematrix!(a)
+   # @show A
+
+   a = SysmatAssemblerSparseSymm(0.0)
+   startassembly!(a, 5 * 5 * 3, 7, 7)
+   m = [0.24406   0.599773    0.833404  0.0420141
+    0.786024  0.00206713  0.995379  0.780298
+    0.845816  0.198459    0.355149  0.224996]
+    # @show    m'*m
+   assemble!(a, m'*m, [5 2 1 4], [5 2 1 4])
+   m = [0.146618  0.53471   0.614342    0.737833
+     0.479719  0.41354   0.00760941  0.836455
+     0.254868  0.476189  0.460794    0.00919633
+     0.159064  0.261821  0.317078    0.77646
+     0.643538  0.429817  0.59788     0.958909]
+     # @show    m'*m
+   assemble!(a, m'*m, [2 3 1 5], [2 3 1 5])
+   A1 = makematrix!(a)
+   @test norm(A - A1) / norm(A1) < 1.0e-9
+   return true
+end
+
+_test()
+
+end # module
