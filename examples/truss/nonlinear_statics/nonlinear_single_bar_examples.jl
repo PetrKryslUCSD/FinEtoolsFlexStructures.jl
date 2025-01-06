05357e1e98d593de4677f84e5d3828df2dd7a988
Petr Krysl <pkrysl@ucsd.edu>
2025-01-05 16:27:52 -0800

    add riks sol

--- examples/truss/nonlinear_statics/nonlinear_single_bar_examples.jl
+++ examples/truss/nonlinear_statics/nonlinear_single_bar_examples.jl
@@ -194,10 +194,194 @@ function solve(visualize=false)
     true
 end # force_1
 
+function solve_riks(visualize=false)
+    # Parameters:
+    E = 30_000.0 * phun("MPa")
+    nu = 0.3
+    W = 2500.0 * phun("mm")
+    H = 25.0 * phun("mm")
+    x = Float64[
+        0.0 0.0 0.0 # 1
+        W H 0.0  # 2
+    ] 
+    area = 5e7 / E
+    P = 0.01 * phun("kilo*N")
+    maxstep = 10
+    maxit = 30
+    rtol = 1e-9
+    disp_mag = H / 5 # vertical coordinate of the crown
+    step_length = 3.3
+    analytical(w) = -E * area * (H + w) * (2 * (H + w) * w - w^2) / (2 * W^3)
+
+    # Cross-sectional properties
+    cs = CrossSectionRectangle(s -> sqrt(area), s -> sqrt(area), s -> [0.0, 0.0, -1.0])
+
+    # Select the number of elements per leg.
+    n = 1
+    members = []
+    for j in (
+        [1, 2],
+    )
+        push!(members, frame_member(x[j, :], n, cs))
+    end
+    fens, fes = merge_members(members; tolerance=1 / 1000)
+
+    # Material properties
+    material = MatDeforElastIso(DeforModelRed3D, E, nu)
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz, 1), 3))
+    Rfield0 = initial_Rfield(fens)
+    Rfield1 = deepcopy(Rfield0) # not used by the truss mechanics, but it needs to be present
+    dchi = NodalField(zeros(size(fens.xyz, 1), 3))
+
+    # Apply EBC's
+    pinned = selectnode(fens; box = boundingbox(x[1, :]), tolerance = H/100)
+    for i in [1, 2, 3,]
+        setebc!(dchi, pinned, true, i)
+    end
+    loaded = selectnode(fens; box = boundingbox(x[2, :]), tolerance = H/100)
+    for i in [1, 3,]
+        setebc!(dchi, loaded, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi)
+    fr = freedofs(dchi)
+    
+    # Assemble the global discrete system
+    # massem = SysmatAssemblerFFBlock(nfreedofs(dchi))
+    # vassem = SysvecAssemblerFBlock(nfreedofs(dchi))
+
+    femm = FEMMCorotTruss(IntegDomain(fes, GaussRule(1, 2)), material)
+    
+    K = stiffness(femm, geom0, u0, Rfield0, dchi)
+    loadbdry = FESetP1(reshape(loaded, 1, 1))
+    lfemm = FEMMBase(IntegDomain(loadbdry, PointRule()))
+    q = zeros(3)
+    q[2] = -P
+    fi = ForceIntensity(q)
+    F = distribloads(lfemm, geom0, dchi, fi, 3)
+    
+    # Solve the static problem
+    
+    F = F[fr]
+
+    
+    # Auxiliary Variables 
+    u1 = deepcopy(u0)
+    dchiv = fill(0.0, length(fr))
+    chivc = fill(0.0, length(fr))
+    chivc1 = fill(0.0, length(fr))
+    dchivprev = fill(0.0, length(fr))
+
+    lam = 0.0
+    lamprev = 0.0
+
+    tip_displacement = Float64[0.0]
+    actual_lams = Float64[0.0]
+
+    step = 1
+    while step <= maxstep
+        u0.values[:] .= u1.values[:]
+        applyebc!(dchi) # Apply boundary conditions
+
+        println("Step: $step, starting from load parameter $lam")
+        println("===========================================================")
+
+        K = stiffness(femm, geom0, u1, Rfield1, dchi) + geostiffness(femm, geom0, u1, Rfield1, dchi)
+        chivc1 .= K[fr, fr] \ F
+        @show lam, lamprev
+        @show dellam1 = step_length / sqrt(dot(chivc1, chivc1) / disp_mag^2 + 1)
+        @show dircoeff = dellam1 * (dot(chivc1, dchivprev) / disp_mag^2 + (lam - lamprev))
+        if dircoeff < 0.0
+            dellam1 = -dellam1
+        end
+        lamprev = lam
+        lam += dellam1
+
+        dchiv .= dchivprev 
+        u1.values[:] .= u0.values[:] .+ dchi.values[:]   # increment displacement7
+        
+        converged = true
+        error = 1.0
+        iter = 0
+        while true
+            iter += 1
+            Fr = restoringforce(femm, geom0, u1, Rfield1, dchi)[fr]       # Internal forces
+            res = lam * F + Fr
+        
+            error = norm(res) / norm(F)
+            @info "Iter $iter, lam = $lam, bal. error $error"
+
+            if error < rtol
+                break
+            end
+
+            K = stiffness(femm, geom0, u1, Rfield1, dchi) + geostiffness(femm, geom0, u1, Rfield1, dchi)
+            chivc .= K[fr, fr] \ F
+            ddchiv = K[fr, fr] \ res
+
+            mu = - (dot(ddchiv, chivc1) / disp_mag^2) / (dot(chivc, chivc1) / disp_mag^2 + 1)
+            
+            lam += mu
+
+            dchiv .+= ddchiv + mu .* chivc
+
+            dchi = scattersysvec!(dchi, dchiv)
+            u1.values[:] .= u0.values[:] .+ dchi.values[:]   # increment displacement
+
+            if iter > maxit
+                converged = false
+                break
+            end
+        end
+        
+        if !converged
+            @warn "Nonlinear iteration did not converge"
+            break
+        end
+
+        dchivprev .= dchiv
+        
+        # Store output from step
+        push!(actual_lams, lam)
+        push!(tip_displacement, u1.values[loaded[1], 2])
+
+        step += 1
+    end
+    
+    tip_displacement = vec(tip_displacement)
+    plots = cat(
+    scatter(;x=vec(tip_displacement) ./ phun("mm"), y=analytical.(tip_displacement), mode="lines", line_color = "rgb(0, 0, 0)", name="Analytical"),
+    scatter(;x=tip_displacement ./ phun("mm"), y=P .* actual_lams, mode="markers", line_color = "rgb(255, 0, 0)", name="FEA"),
+    dims = 1)
+    layout = Layout(width=700, height=500, 
+        xaxis=attr(title="Tip displacement [mm]"),
+        yaxis=attr(title="Load [N]"))
+    pl = plot(plots, layout)
+    display(pl)
+
+    if visualize
+        scaling = 1e1
+        dchi.values .*= scaling
+        radius = 20 * phun("in")
+        plots = cat(plot_space_box([[-radius -radius -radius]; [radius radius radius]]),
+                    plot_nodes(fens),
+                    plot_solid(fens, fes;
+                               x = geom0.values, u = u1.values[:, 1:3], );
+                    dims = 1)
+        pl = render(plots)
+    end
+    
+    true
+end # force_1
+
 function allrun()
     println("#####################################################")
     println("# solve ")
-    solve(false)
+    solve_riks(false)
     return true
 end # function allrun
 
