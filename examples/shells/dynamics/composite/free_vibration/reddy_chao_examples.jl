703317c36a21b613facda2bccb2f8fb8f98bcc95
Petr Krysl <pkrysl@ucsd.edu>
2025-02-22 08:10:57 -0800

    fix chao

--- examples/shells/dynamics/composite/free_vibration/reddy_chao_examples.jl
+++ examples/shells/dynamics/composite/free_vibration/reddy_chao_examples.jl
@@ -1,7 +1,8 @@
 """
-Vibration analysis of anisotropic layered square plates
+Vibration analysis of cross-ply layered square plates
 
 Layup [0/90/0] (thicknesses t/4, t/2, t/4).
+Material II.
 
 Reference values: Table 6 (page 163)
 From: J.N. REDDY and W.C. CHAO 
@@ -23,18 +24,18 @@ using FinEtoolsFlexStructures.CompositeLayupModule
 using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, update_rotation_field!
 using VisualStructures: plot_nodes, plot_midline, render, plot_space_box, plot_midsurface, space_aspectratio, save_to_json
 
-function _execute(aspect, n, visualize)
+function _execute(aspect, n, reference, visualize)
     formul = FEMMShellT3FFCompModule
     CM = CompositeLayupModule
-    # Material I
-    rho = 2250*phun("KG/M^3")
-    E2 = 3*phun("GPa")
-    E1 = E2 * 25
-    G12 = E2 * 0.5
-    G13 = G12
-    G23 = E2 * 0.2
-    nu12 = 0.25
-    Material_I = (rho, E1, E2, nu12, G12, G13, G23)
+    # # Material I
+    # rho = 2250*phun("KG/M^3")
+    # E2 = 3*phun("GPa")
+    # E1 = E2 * 25
+    # G12 = E2 * 0.5
+    # G13 = G12
+    # G23 = E2 * 0.2
+    # nu12 = 0.25
+    # Material_I = (rho, E1, E2, nu12, G12, G13, G23)
     # Material II
     rho = 2250*phun("KG/M^3")
     E2 = 3*phun("GPa")
@@ -45,7 +46,7 @@ function _execute(aspect, n, visualize)
     nu12 = 0.25
     Material_II = (rho, E1, E2, nu12, G12, G13, G23)
 
-    @show Material = Material_II
+    Material = Material_II
    
     L0 = 0.3 * phun("m")
     thickness = L0 / aspect
@@ -65,7 +66,7 @@ function _execute(aspect, n, visualize)
     fens.xyz = xyz3(fens)
     fens, fes = Q4toT3(fens, fes)
     
-    vtkwrite("reddy_chao_n=$n.vtu", fens, fes)
+    # vtkwrite("reddy_chao_n=$n.vtu", fens, fes)
 
     mater = CM.lamina_material(Material...)
     plies = CM.Ply[
@@ -94,10 +95,21 @@ function _execute(aspect, n, visualize)
     # Apply EBC's
     # simple support
     l1 = connectednodes(meshboundary(fes))
-    for i in [1, 2, 3, ]
+    for i in  [3, 6] 
         setebc!(dchi, l1, true, i)
     end
-    
+    l1 = selectnode(fens, box=Float64[0, 0, -Inf, Inf, 0, 0], inflate=tolerance)
+    setebc!(dchi, l1, true, 2)
+    setebc!(dchi, l1, true, 4)
+    l1 = selectnode(fens, box=Float64[L0, L0, -Inf, Inf, 0, 0], inflate=tolerance)
+    setebc!(dchi, l1, true, 2)
+    setebc!(dchi, l1, true, 4)
+    l1 = selectnode(fens, box=Float64[-Inf, Inf, 0, 0, 0, 0], inflate=tolerance)
+    setebc!(dchi, l1, true, 1)
+    setebc!(dchi, l1, true, 5)
+    l1 = selectnode(fens, box=Float64[-Inf, Inf, L0, L0, 0, 0], inflate=tolerance)
+    setebc!(dchi, l1, true, 1)
+    setebc!(dchi, l1, true, 5)
     applyebc!(dchi)
     numberdofs!(dchi);
 
@@ -116,8 +128,9 @@ function _execute(aspect, n, visualize)
     d[:] = d .- OmegaShift;
     oms = real(sqrt.(complex(d)))
     fs = oms ./(2*pi)
-    @info "Frequencies: $fs [Hz]"
-    @info "Nondimensional angular frequencies\n  $(nondimensionalised_frequency.(oms))"
+    # @info "Frequencies: $fs [Hz]"
+    ndom = nondimensionalised_frequency.(oms[1])
+    @info "Nondim freq: $(ndom) ($(ndom/reference*100) %)"
         
     # Visualization
     if visualize
@@ -135,10 +148,16 @@ end
 
 
 function test_convergence()
-    @info "Reddy-Chao plate vibration"
-    for aspect in [2, 5, 10, 25, 50]
-        for n in [100]
-            _execute(aspect, n, false)
+    reference = Dict(
+        "a/h=5" => 10.861,
+        "a/h=10" => 15.156,
+        "a/h=25" => 18.089,
+        "a/h=50" => 18.698,
+    )
+    @info "Reddy-Chao square plate vibration"
+    for aspect in [5, 10, 25, 50]
+        for n in [10, 20, 40]
+            _execute(aspect, n, reference["a/h=$aspect"], false)
         end
     end
     return true
