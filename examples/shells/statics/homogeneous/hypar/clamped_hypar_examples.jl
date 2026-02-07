9305b74c5837afbc820cbc17b61b1f72d477adbb
Petr Krysl <pkrysl@ucsd.edu>
2026-02-07 09:20:21 -0800

    add hypar example

--- examples/shells/statics/homogeneous/hypar/clamped_hypar_examples.jl
+++ examples/shells/statics/homogeneous/hypar/clamped_hypar_examples.jl
@@ -31,11 +31,12 @@ using FinEtoolsDeforLinear
 using FinEtoolsFlexStructures.FESetShellT3Module: FESetShellT3
 using FinEtoolsFlexStructures.FESetShellQ4Module: FESetShellQ4
 using FinEtoolsFlexStructures.FEMMShellT3FFModule
+using FinEtoolsFlexStructures.FEMMShellQ4RNTModule
 using FinEtoolsFlexStructures.RotUtilModule: initial_Rfield, update_rotation_field!
 using VisualStructures: plot_nodes, plot_midline, render, plot_space_box, plot_midsurface, space_aspectratio, save_to_json
 using FinEtools.MeshExportModule.VTKWrite: vtkwrite
 
-function _execute(tL_ratio = 1/100, g = 80*0.1^0, analyt_sol=-9.3355e-5, n = 32, visualize = false)
+function _execute_t3ff(tL_ratio = 1/100, g = 80*0.1^0, analyt_sol=-9.3355e-5, n = 32, visualize = false)
     # analytical solution for the vertical deflection and the midpoint of the
     # free edge 
     # Parameters:
@@ -127,6 +128,98 @@ function _execute(tL_ratio = 1/100, g = 80*0.1^0, analyt_sol=-9.3355e-5, n = 32,
     return targetu
 end
 
+function _execute_q4rnt(tL_ratio = 1/100, g = 80*0.1^0, analyt_sol=-9.3355e-5, n = 32, visualize = false)
+    # analytical solution for the vertical deflection and the midpoint of the
+    # free edge 
+    # Parameters:
+    E = 2.0e11;
+    nu = 0.3;
+    L = 1.0
+    thickness = tL_ratio * L
+    # Bathe, Iosilevich, and Chapelle (2000) with a refined mesh of
+    # high-order element MITC16
+    formul = FEMMShellQ4RNTModule
+
+    @info "Mesh: $n elements per side"
+
+    tolerance = L/n/1000
+    fens, fes = Q4block(L,L,n,n);
+    fens.xyz = xyz3(fens)
+    for i in 1:count(fens)
+        x=fens.xyz[i, 1]-L/2; y=fens.xyz[i, 2]-L/2;
+        fens.xyz[i, :] .= (x, y, x^2 - y^2)
+    end
+
+    mater = MatDeforElastIso(DeforModelRed3D, E, nu)
+    
+    sfes = FESetShellQ4()
+    accepttodelegate(fes, sfes)
+    femm = formul.make(IntegDomain(fes, CompositeRule(GaussRule(2, 2), GaussRule(2, 1)), thickness), mater)
+    femm.drilling_stiffness_scale = 0.1
+    stiffness = formul.stiffness
+    associategeometry! = formul.associategeometry!
+
+    # Construct the requisite fields, geometry and displacement
+    # Initialize configuration variables
+    geom0 = NodalField(fens.xyz)
+    u0 = NodalField(zeros(size(fens.xyz,1), 3))
+    Rfield0 = initial_Rfield(fens)
+    dchi = NodalField(zeros(size(fens.xyz,1), 6))
+
+    # Apply EBC's
+    # Clamped edge
+    l1 = selectnode(fens; box = Float64[-L/2 -L/2 -Inf Inf -Inf Inf], inflate = tolerance)
+    for i in [1,2,3,4,5,6]
+        setebc!(dchi, l1, true, i)
+    end
+    applyebc!(dchi)
+    numberdofs!(dchi);
+
+    massem = SysmatAssemblerFFBlock(nfreedofs(dchi))
+    vassem = SysvecAssemblerFBlock(nfreedofs(dchi))
+
+    # Assemble the system matrix
+    associategeometry!(femm, geom0)
+    Kff = stiffness(femm, massem, geom0, u0, Rfield0, dchi);
+
+    # Midpoint of the free edge
+    nl = selectnode(fens; box = Float64[L/2 L/2 0 0 -Inf Inf], inflate = tolerance)
+    lfemm = FEMMBase(IntegDomain(fes, GaussRule(2, 2)))
+    # computeforce!(forceout, XYZ, tangents, fe_label) = let
+    #     n = cross(tangents[:, 1], tangents[:, 2])
+    #     n = n / norm(n)
+    #     forceout .= 0.0
+    #     forceout[3] = -g * n[3]
+    #     return forceout
+    # end
+    # fi = ForceIntensity(Float64, 6, computeforce!)
+    fi = ForceIntensity(Float64[0, 0, -g, 0, 0, 0]);
+    Ff = distribloads(lfemm, vassem, geom0, dchi, fi, 3);
+    
+    # Solve
+    Uf = Kff \ Ff
+    scattersysvec!(dchi, Uf, DOF_KIND_FREE)
+    U = gathersysvec(dchi, DOF_KIND_ALL)
+    
+    targetu = dchi.values[nl, 3][1]
+    @info "Displacement Solution: $(round(targetu/analyt_sol, digits = 4)*100)%"
+    @info "Strain Energy Solution: $(Uf'*Ff/2)"
+
+        # Visualization
+    if visualize
+        vtkwrite("clamped_hypar_examples-geometry.vtu", fens, fes)
+        scattersysvec!(dchi, (L/4)/abs(targetu).*U)
+        update_rotation_field!(Rfield0, dchi)
+        plots = cat(plot_space_box([[0 0 -L/2]; [L/2 L/2 L/2]]),
+        #plot_nodes(fens),
+            plot_midsurface(fens, fes; x = geom0.values, facecolor = "rgb(12, 12, 123)"),
+            plot_midsurface(fens, fes; x = geom0.values, u = dchi.values[:, 1:3], R = Rfield0.values);
+            dims = 1)
+        pl = render(plots)
+    end
+    return targetu
+end
+
 function test_convergence()
     
     tL_ratios = [1/100, 1/1000, 1/10000]; 
@@ -134,11 +227,22 @@ function test_convergence()
     analyt_sols = [-9.3355e-5, -6.3941e-3, -5.2988e-1];
     
     for (tL_ratio, g, analyt_sol) in zip(tL_ratios, gs, analyt_sols)
-        @info "Clamped hypar, t/L=$(tL_ratio)"
+        @info "Clamped hypar, t/L=$(tL_ratio), T3FF elements"
         results = Float64[]
-        for n in [4, 8, 16, 32, 64, 128, 256, ]
+        for n in [4, 8, 16, 32, 64, ]
         # for n in [4, 8, 16, 32, 64, ]
-            r = _execute(tL_ratio, g, analyt_sol, n, false)
+            r = _execute_t3ff(tL_ratio, g, analyt_sol, n, false)
+            push!(results, r/analyt_sol)
+        end   
+        @show results
+    end
+
+    for (tL_ratio, g, analyt_sol) in zip(tL_ratios, gs, analyt_sols)
+        @info "Clamped hypar, t/L=$(tL_ratio), Q4RNT elements"
+        results = Float64[]
+        for n in [4, 8, 16, 32, 64, ]
+        # for n in [4, 8, 16, 32, 64, ]
+            r = _execute_q4rnt(tL_ratio, g, analyt_sol, n, false)
             push!(results, r/analyt_sol)
         end   
         @show results
